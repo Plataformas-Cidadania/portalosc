1a3f86068d1f4c3f2cb150f74fcf81dfe685eb21
Vagner Praia da Silva <vagnerpraia@gmail.com>
2016-06-07 22:33:27 -0300

    Adicionando tratador global de exceções

--- admin/global.js
+++ admin/global.js
@@ -4,6 +4,10 @@ function setGlobal(rootPath){
 	global.rootRequire = function(name) {
 	    return require(rootPath + '/' + name);
 	}
+	
+	process.on('uncaughtException', function(error){
+		console.log('ocorreu um erro.');
+	});
 }
 
 module.exports = {  
