01ec65892e5b1b9888cfa33da7a04ef81e9fe53f
Rafael Lage Tavares <rlt.rafael@gmail.com>
2016-06-13 15:26:58 -0300

    Issue: MOSC#580

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 var express = require('express'),
-    router  = express.Router();
+  path = require('path'),
+  router  = express.Router();
 
 router.use(require('./osc'));
 router.use(require('./user'));
+
+router.get('/', function(req,res){
+  res.sendfile(path.resolve('client/html/index.html'));
+});
 
 module.exports = router;
