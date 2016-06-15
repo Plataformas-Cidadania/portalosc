5354797eb3e2382138e4ec3d564f5e650f656fd5
vagnerpraia <vagnerpraia@pc-vagnerpraia>
2016-06-14 21:29:17 -0300

    Issue: MOSC-603 Subject: Funções para gravar os dados da página de edição da OSC Detail: Implementação da função update para atualização dos dados de OSC

--- odbc/osc/postgres.js
+++ odbc/osc/postgres.js
@@ -22,6 +22,36 @@ function getOSC(id, callback){
 	});
 };
 
+function updateOSC(osc, callback){
+	pg.connect(con, function(error, client, done) {
+		if(error) {
+			console.error('error fetching client from pool', error);
+			callback(true);
+		}
+		
+		sql = 'UPDATE portal.vm_osc_principal SET ' +
+			  'bosc_nm_fantasia_osc = ' + osc.nome_fantasia + ', ' +
+			  'ospr_tx_descricao = ' + osc.descricao + ', ' +
+			  'ospr_dt_ano_fundacao = ' + osc.ano_fundacao + ', ' +
+			  'ospr_ee_site = ' + osc.site + ', ' +
+			  'ee_google = ' + osc.google + ', ' +
+			  'ee_facebook = ' + osc.facebook + ', ' +
+			  'ee_linkedin = ' + osc.linkedin + ', ' +
+			  'ee_twitter = ' + osc.twitter + ', ' +
+			  'WHERE bosc_sq_osc = ' + osc.id;
+		
+		client.query(sql, function(error, result) {
+			done();
+			if(error) {
+				console.error('error running query', error);
+				callback(true);
+			}
+			callback(false);
+		});
+	});
+};
+
 module.exports = {  
-	getOSC: getOSC
+	getOSC: getOSC,
+	updateOSC: updateOSC
 }
