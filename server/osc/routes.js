613e7e27bdc21889f7bd4a1f122349821a296b54
EricFerreiraS <eric@wiiglo.com>
2016-06-16 11:46:51 -0300

    Issue: [MOSC #611] Subject:Modificação da estrutura do arquivo Detail:

--- server/osc/routes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express'),
-	services = require('./services'),
-	router  = express.Router();
-	
-router.get('/osc/:id', services.getOSC);
-
-module.exports = {
-	router: router
-}
