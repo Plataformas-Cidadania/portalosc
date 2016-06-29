7ce7fe67c9d992297b0ac19e87c3bb47e7d804b3
EricFerreiraS <eric@wiiglo.com>
2016-06-28 22:26:30 -0300

    Issue: [MOSC #523] Subject:Alteracao da estrutura Detail: Usando estrutura do Lumen/Laravel

--- app/client/js/index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  var goButton = document.getElementsByClassName('btn-primary')[0];
-  goButton.onclick = function(){
-    var oscId = document.getElementById('oscIdElement').value;
-    window.location.replace('html/paginaOsc.html?id=' + oscId);
-  }
-});
--- app/odbc/index.js
+++
@@ -1,5 +0,0 @@
-var express = require('express');
-
-module.exports = {
-	osc: require('./osc')
-}
