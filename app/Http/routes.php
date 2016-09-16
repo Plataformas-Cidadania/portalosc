03a3f731c1195df360de1f47911da4d8caec304a
Denise <dssilva.2689@gmail.com>
2016-09-16 18:07:24 -0300

    Issue: MOSC-609 Subject: Atualização da função GetOSC Detail:

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,6 +2,19 @@
 $app->get('/', function () use ($app) {
 	return 'Mapa das Organizações da Sociedade Civil';
 });
+
+$app->group(['prefix' => 'api/osc'], function () use ($app) {
+	$app->get('cabecalho/{id}', 'App\Http\Controllers\OscController@getCabecalhoOsc');
+	$app->get('dadosgerais/{id}', 'App\Http\Controllers\OscController@getDadosGerais');
+	$app->get('areasatuacao/{id}', 'App\Http\Controllers\OscController@getAreasAtuacao');
+	$app->get('descricao/{id}', 'App\Http\Controllers\OscController@getDescricao');
+	$app->get('titulacoescertificacoes/{id}', 'App\Http\Controllers\OscController@getTitulacoesCertificacoes');
+	$app->get('colaboradores/{id}', 'App\Http\Controllers\OscController@getColaboradores');
+	$app->get('diretores/{id}', 'App\Http\Controllers\OscController@getDiretores');
+	$app->get('recursos/{id}', 'App\Http\Controllers\OscController@getRecursos');
+	$app->get('projetos/{id}', 'App\Http\Controllers\OscController@getProjetos');
+	$app->get('espacosparticipacao/{id}', 'App\Http\Controllers\OscController@getEspacosParticipacao');
+});
+
 $app->get('organization/id/{id}', 'OscController@getOsc');
-$app->get('dadosgerais/id/{id}', 'OscController@getDadosGerais');
 $app->get('dadosteste/id/{id}', 'OscController@getDadosTeste');
