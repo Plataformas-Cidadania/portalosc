ece3b3dcedcdebd0a19454cfaf24474f719cbe52
vagnerpraia <vagnerpraia@gmail.com>
2016-09-19 18:51:53 -0300

    Issue: MOSC-609 Subject: Atualização da função GetOSC Detail: Remoção de função de teste e atualizações

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,7 +1,5 @@
 <?php
 $app->group(['prefix' => 'api/osc'], function () use ($app) {
-	$app->get('{id}', 'App\Http\Controllers\OscController@getOsc2');
+	$app->get('{id}', 'App\Http\Controllers\OscController@getOsc');
 	$app->get('{component}/{id}', 'App\Http\Controllers\OscController@getComponentOsc');
 });
-
-$app->get('organization/id/{id}', 'OscController@getOsc');
