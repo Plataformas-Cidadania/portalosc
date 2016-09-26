1d46a33581f33073b0e90b6636332e98d6e9e1b0
vagnerpraia <vagnerpraia@gmail.com>
2016-09-26 14:44:12 -0300

    Atualização da view e da function de recursos

--- artefacts/database/model/Functions/osc/get_osc_recursos.sql
+++ artefacts/database/model/Functions/osc/get_osc_recursos.sql
@@ -1,10 +1,15 @@
 ﻿CREATE OR REPLACE FUNCTION portal.get_osc_recursos(id_request INTEGER) RETURNS TABLE (
 	id_osc INTEGER,
 	nr_valor_total DOUBLE PRECISION,
+	nr_valor_federal DOUBLE PRECISION,
+	nr_valor_estadual DOUBLE PRECISION,
+	nr_valor_municipal DOUBLE PRECISION,
+	nr_valor_privado DOUBLE PRECISION,
 	tx_link_relatorio_auditoria TEXT,
 	ft_link_relatorio_auditoria TEXT,
 	tx_link_demonstracao_contabil TEXT,
-	ft_link_demonstracao_contabil TEXT
+	ft_link_demonstracao_contabil TEXT,
+	nr_valor_proprio TEXT
 ) AS $$
 BEGIN
 	RETURN QUERY
--- artefacts/database/model/mapaosc_2.sql
+++ artefacts/database/model/mapaosc_2.sql
@@ -2098,6 +2098,31 @@ AS
 SELECT
 	tb_dados_gerais.id_osc,
 	(SELECT sum(tb_projeto.nr_valor_total_projeto) FROM osc.tb_projeto WHERE tb_projeto.id_osc = tb_osc.id_osc) AS nr_valor_total,
+	(SELECT sum(projeto.nr_valor_total_projeto)	FROM osc.tb_projeto AS projeto INNER JOIN osc.tb_fonte_recursos_projeto AS recursos	ON projeto.id_projeto = recursos.id_projeto	WHERE (
+		SELECT tx_nome_fonte_recursos
+		FROM syst.dc_fonte_recursos
+		WHERE cd_fonte_recursos = recursos.cd_fonte_recursos
+	) = 'Público Federal') AS nr_valor_federal,
+	(SELECT sum(projeto.nr_valor_total_projeto)	FROM osc.tb_projeto AS projeto INNER JOIN osc.tb_fonte_recursos_projeto AS recursos	ON projeto.id_projeto = recursos.id_projeto	WHERE (
+		SELECT tx_nome_fonte_recursos
+		FROM syst.dc_fonte_recursos
+		WHERE cd_fonte_recursos = recursos.cd_fonte_recursos
+	) = 'Público Estadual') AS nr_valor_estadual,
+	(SELECT sum(projeto.nr_valor_total_projeto)	FROM osc.tb_projeto AS projeto INNER JOIN osc.tb_fonte_recursos_projeto AS recursos	ON projeto.id_projeto = recursos.id_projeto	WHERE (
+		SELECT tx_nome_fonte_recursos
+		FROM syst.dc_fonte_recursos
+		WHERE cd_fonte_recursos = recursos.cd_fonte_recursos
+	) = 'Público Municipal') AS nr_valor_municipal,
+	(SELECT sum(projeto.nr_valor_total_projeto)	FROM osc.tb_projeto AS projeto INNER JOIN osc.tb_fonte_recursos_projeto AS recursos	ON projeto.id_projeto = recursos.id_projeto	WHERE (
+		SELECT tx_nome_fonte_recursos
+		FROM syst.dc_fonte_recursos
+		WHERE cd_fonte_recursos = recursos.cd_fonte_recursos
+	) = 'Privado') AS nr_valor_privado,
+	(SELECT sum(projeto.nr_valor_total_projeto)	FROM osc.tb_projeto AS projeto INNER JOIN osc.tb_fonte_recursos_projeto AS recursos	ON projeto.id_projeto = recursos.id_projeto	WHERE (
+		SELECT tx_nome_fonte_recursos
+		FROM syst.dc_fonte_recursos
+		WHERE cd_fonte_recursos = recursos.cd_fonte_recursos
+	) = 'Próprio') AS nr_valor_proprio,
 	tb_dados_gerais.tx_link_relatorio_auditoria,
 	tb_dados_gerais.ft_link_relatorio_auditoria,
 	tb_dados_gerais.tx_link_demonstracao_contabil,
