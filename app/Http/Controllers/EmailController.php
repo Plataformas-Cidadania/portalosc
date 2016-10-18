369a7a8031585e12ddfbd9175ac76c971790896e
Denise <Denise@192.168.1.2>
2016-10-18 12:17:00 -0200

    Issue: MOSC-814 Subject: Criar função para enviar email Detail:

---
+++ app/Http/Controllers/EmailController.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Mail;
+use PEAR;
+
+class EmailController extends Controller {
+	
+    public function send($email, $name, $text_subject, $message)
+    {  	
+    	$host = env('MAIL_HOST');
+		$username = env('MAIL_USERNAME');
+		$password = env('MAIL_PASSWORD');
+		$port = env('MAIL_PORT');
+		$from = env('MAIL_FROM');
+		
+		$to = $email;
+		$subject = $text_subject ;
+		$body = $message;
+		
+		$headers = array ('Content-type' => 'text/html; charset=UTF-8', 'From' => $from, 'To' => $to, 'Subject' => $subject);
+		
+		$smtp = Mail::factory('smtp', array ('host' => $host, 'port' => $port, 'auth' => true, 'username' => $username, 'password' => $password));
+		
+		$mail = $smtp->send($to, $headers, $body);
+		
+		if (PEAR::isError($mail)) {
+			echo("<p>" . $mail->getMessage() . "</p>");
+		} else {
+			echo("<p>Email enviado com sucesso!</p>");
+		}
+    }
+    
+    public function date()
+    {
+    	setlocale(LC_ALL, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
+    	date_default_timezone_set('America/Sao_Paulo');
+    	$date = utf8_encode(strftime('%d').' de '.ucwords(strftime('%B')).' de '.strftime('%Y'));
+    	
+    	return $date;
+    }
+   
+    public function welcome($nome)
+    {
+    	return
+    	'<html>
+		<head>
+		<title>E-mail Boas Vindas</title>
+		</head>
+		<body bgcolor="#FFFFFF" style="margin: 0 auto; font-size: 16px;">
+		<table id="Table_01" border="0" align="center" cellpadding="0" cellspacing="0" style="border: 2px solid #f4f4f4; min-width:300px; width:100%; max-width:700px; margin:20px auto;">
+		<tbody>
+		<tr>
+		<td colspan="3" style="padding:20px;">
+		<img src="https://mapaosc.ipea.gov.br/imagens/logo.png" height="97" alt=""/>
+		</td>
+		</tr>
+		<tr>
+		<td height="27" colspan="3" bgcolor="#F4F4F4" style="padding:10px 20px;">
+		<h1 style="padding: 0.5em;margin: 0;"><font size="6" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Obrigado por se cadastrar!</font></h1>
+		</td>
+		</tr>
+		<tr>
+		<td  colspan="3" bgcolor="#FFFFFF" style="padding:20px;">
+		<p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Olá, '.$nome.'!</font> </p>
+		<p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Obrigado por se cadastrar no MAPA das Organizações da Sociedade Civil.</font> </p>
+		</td>
+		</tr>
+		<tr>
+		<td width="auto"></td>
+		<td valign="middle" align="right" style="padding:20px;">
+		<img src="https://mapaosc.ipea.gov.br/imagens/loading.png" height="71" width="71" alt=""/>
+		</td>
+		<td width="420" bgcolor="#FFFFFF" valign="middle" style="padding: 20px 0;">
+		<p style="text-align: justify; margin: 0;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Agradecemos pelo contato,</font> </p>
+		<p style="text-align: justify; margin: 0;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Equipe de Desenvolvimento do Mapa das OSCs</font> </p>
+		<p style="text-align: justify; margin: 0;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif"><a href="https://mapaosc.ipea.gov.br">Mapa das OSCs</a> - '.$this->date().'.</font> </p>
+		</td>
+		</tr>
+		</tbody>
+		</table>
+		</body>
+		</html>';
+    }
+    
+    public function confirmation($name, $token) 
+    {
+    	return
+    	'<html>
+    	<head>
+    	<title>E-mail Confirmacao Cadastro</title>
+    	</head>
+    	<body bgcolor="#FFFFFF" style="margin: 0 auto; font-size: 16px;">
+    	<table id="Table_01" border="0" align="center" cellpadding="0" cellspacing="0" style="border: 2px solid #f4f4f4; min-width:300px; width:100%; max-width:700px; margin:20px auto;">
+    	<tbody>
+    	<tr>
+    	<td colspan="3" style="padding:20px;">
+    	<img src="https://mapaosc.ipea.gov.br/imagens/logo.png" height="97" alt=""/>
+    	</td>
+    	</tr>
+    	<tr>
+    	<td height="27" colspan="3" bgcolor="#F4F4F4" style="padding:10px 20px;">
+    	<h1 style="padding: 0.5em;margin: 0;"><font size="6" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Confirmação de Cadastro</font></h1>
+    	</td>
+    	</tr>
+    	<tr>
+    	<td  colspan="3" bgcolor="#FFFFFF" style="padding:20px;">
+    	<p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Olá, '.$name.'!</font> </p>
+    	<p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Estamos prontos para ativar sua conta. Clique no link abaixo para ativar seu cadastro.</font> </p>
+    	<p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif"><a href="https://mapaosc.ipea.gov.br/Map.html#T'.$token.'">https://mapaosc.ipea.gov.br/Map.html#T'.$token.'</a> </font> </p>
+    	<p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Se você não tiver criado uma conta do Mapa das OSC, desconsidere este email.</font> </p>
+    	</td>
+    	</tr>
+    	<tr>
+    	<td width="auto"></td>
+    	<td valign="middle" align="right" style="padding:20px;">
+    	<img src="https://mapaosc.ipea.gov.br/imagens/loading.png" height="71" width="71" alt=""/>
+    	</td>
+    	<td width="420" bgcolor="#FFFFFF" valign="middle" style="padding: 20px 0;">
+    	<p style="text-align: justify; margin: 0;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Agradecemos pelo contato,</font> </p>
+    	<p style="text-align: justify; margin: 0;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Equipe de Desenvolvimento do Mapa das OSCs</font> </p>
+    	<p style="text-align: justify; margin: 0;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif"><a href="https://mapaosc.ipea.gov.br">Mapa das OSCs</a> - '.$this->date().'.</font> </p>
+    	</td>
+    	</tr>
+    	</tbody>
+    	</table>
+    	</body>
+    	</html>';
+    }
+
+    public function changePassword($name, $token) 
+    {
+    	return
+    	'<html>
+    	<head>
+    	<title>Criar Nova Senha</title>
+    	</head>
+    	<body bgcolor="#FFFFFF" style="margin: 0 auto; font-size: 16px;">
+    	<table id="Table_01" border="0" align="center" cellpadding="0" cellspacing="0" style="border: 2px solid #f4f4f4; min-width:300px; width:100%; max-width:700px; margin:20px auto;">
+    	<tbody>
+    	<tr>
+    	<td colspan="3" style="padding:20px;">
+    	<img src="https://mapaosc.ipea.gov.br/imagens/logo.png" height="97" alt=""/>
+    	</td>
+    	</tr>
+    	<tr>
+    	<td height="27" colspan="3" bgcolor="#F4F4F4" style="padding:10px 20px;">
+    	<h1 style="padding: 0.5em;margin: 0;"><font size="6" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Criar Nova Senha</font></h1>
+    	</td>
+    	</tr>
+    	<tr>
+    	<td  colspan="3" bgcolor="#FFFFFF" style="padding:20px;">
+    	<p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Olá, '.$name.'!</font> </p>
+    	<p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Para cadastrar sua nova senha, clique no link* abaixo:</font> </p>
+    	<p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif"><a href="https://mapaosc.ipea.gov.br/Map.html#C'.$token.'">https://mapaosc.ipea.gov.br/Map.html#C'.$token.'</a></font> </p>
+    	<p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Mas se não tiver pedido para alterar sua senha, desconsidere este email e continue utilizando sua senha atual.</font> </p>
+    	<p style="text-indent: 2.5em;text-align: justify;"> <font size="2" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">*Este link expira em 24 horas.</font> </p>
+    	</td>
+    	</tr>
+    	<tr>
+    	<td width="auto"></td>
+    	<td valign="middle" align="right" style="padding:20px;">
+    	<img src="https://mapaosc.ipea.gov.br/imagens/loading.png" height="71" width="71" alt=""/>
+    	</td>
+    	<td width="420" bgcolor="#FFFFFF" valign="middle" style="padding: 20px 0;">
+    	<p style="text-align: justify; margin: 0;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Agradecemos pelo contato,</font> </p>
+    	<p style="text-align: justify; margin: 0;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Equipe de Desenvolvimento do Mapa das OSCs</font> </p>
+    	<p style="text-align: justify; margin: 0;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif"><a href="https://mapaosc.ipea.gov.br">Mapa das OSCs</a> - '.$this->date().'.</font> </p>
+    	</td>
+    	</tr>
+    	</tbody>
+    	</table>
+    	</body>
+    	</html>';
+    }
+}
