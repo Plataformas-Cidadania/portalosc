436190ede81ad3a52aae3ec2a561763e0cd1e553
Denise <Denise@192.168.1.3>
2016-10-24 11:11:25 -0200

    Issue: MOSC-814 Subject: Criar função para enviar email Detail:

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -64,7 +64,7 @@ class EmailController extends Controller {
 		<tr>
 		<td  colspan="3" bgcolor="#FFFFFF" style="padding:20px;">
 		<p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Olá, '.$name.'!</font> </p>
-		<p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Obrigado por se cadastrar no MAPA das Organizações da Sociedade Civil.</font> </p>
+		<p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Obrigado por se cadastrar no Mapa das Organizações da Sociedade Civil.</font> </p>
 		</td>
 		</tr>
 		<tr>
@@ -74,7 +74,7 @@ class EmailController extends Controller {
 		</td>
 		<td width="420" bgcolor="#FFFFFF" valign="middle" style="padding: 20px 0;">
 		<p style="text-align: justify; margin: 0;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Agradecemos pelo contato,</font> </p>
-		<p style="text-align: justify; margin: 0;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Equipe de Desenvolvimento do Mapa das OSCs</font> </p>
+		<p style="text-align: justify; margin: 0;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Equipe do Mapa das OSCs</font> </p>
 		<p style="text-align: justify; margin: 0;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif"><a href="https://mapaosc.ipea.gov.br">Mapa das OSCs</a> - '.$this->date().'.</font> </p>
 		</td>
 		</tr>
@@ -109,7 +109,7 @@ class EmailController extends Controller {
     	<p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Olá, '.$name.'!</font> </p>
     	<p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Estamos prontos para ativar sua conta. Clique no link abaixo para ativar seu cadastro.</font> </p>
     	<p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif"><a href="https://mapaosc.ipea.gov.br/Map.html#T'.$token.'">https://mapaosc.ipea.gov.br/Map.html#T'.$token.'</a> </font> </p>
-    	<p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Se você não tiver criado uma conta do Mapa das OSC, desconsidere este email.</font> </p>
+    	<p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Se você não criou uma conta do Mapa das OSC, desconsidere este email.</font> </p>
     	</td>
     	</tr>
     	<tr>
@@ -119,7 +119,7 @@ class EmailController extends Controller {
     	</td>
     	<td width="420" bgcolor="#FFFFFF" valign="middle" style="padding: 20px 0;">
     	<p style="text-align: justify; margin: 0;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Agradecemos pelo contato,</font> </p>
-    	<p style="text-align: justify; margin: 0;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Equipe de Desenvolvimento do Mapa das OSCs</font> </p>
+    	<p style="text-align: justify; margin: 0;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Equipe do Mapa das OSCs</font> </p>
     	<p style="text-align: justify; margin: 0;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif"><a href="https://mapaosc.ipea.gov.br">Mapa das OSCs</a> - '.$this->date().'.</font> </p>
     	</td>
     	</tr>
@@ -154,7 +154,7 @@ class EmailController extends Controller {
     	<p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Olá, '.$name.'!</font> </p>
     	<p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Para cadastrar sua nova senha, clique no link* abaixo:</font> </p>
     	<p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif"><a href="https://mapaosc.ipea.gov.br/Map.html#C'.$token.'">https://mapaosc.ipea.gov.br/Map.html#C'.$token.'</a></font> </p>
-    	<p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Mas se não tiver pedido para alterar sua senha, desconsidere este email e continue utilizando sua senha atual.</font> </p>
+    	<p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Se você não solicitou alteração de senha, desconsidere esta mensagem e continue utilizando a senha atual.</font> </p>
     	<p style="text-indent: 2.5em;text-align: justify;"> <font size="2" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">*Este link expira em 24 horas.</font> </p>
     	</td>
     	</tr>
@@ -165,7 +165,55 @@ class EmailController extends Controller {
     	</td>
     	<td width="420" bgcolor="#FFFFFF" valign="middle" style="padding: 20px 0;">
     	<p style="text-align: justify; margin: 0;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Agradecemos pelo contato,</font> </p>
-    	<p style="text-align: justify; margin: 0;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Equipe de Desenvolvimento do Mapa das OSCs</font> </p>
+    	<p style="text-align: justify; margin: 0;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Equipe do Mapa das OSCs</font> </p>
+    	<p style="text-align: justify; margin: 0;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif"><a href="https://mapaosc.ipea.gov.br">Mapa das OSCs</a> - '.$this->date().'.</font> </p>
+    	</td>
+    	</tr>
+    	</tbody>
+    	</table>
+    	</body>
+    	</html>';
+    }
+    
+    public function informationOSC($user, $nameOSC)
+    {
+    	return
+    	'<html>
+    	<head>
+    	<title>E-mail Informativo a OSC</title>
+    	</head>
+    	<body bgcolor="#FFFFFF" style="margin: 0 auto; font-size: 16px;">
+    	<table id="Table_01" border="0" align="center" cellpadding="0" cellspacing="0" style="border: 2px solid #f4f4f4; min-width:300px; width:100%; max-width:700px; margin:20px auto;">
+    	<tbody>
+    	<tr>
+    	<td colspan="3" style="padding:20px;">
+    	<img src="https://github.com/Plataformas-Cidadania/mapaosc/blob/master/src/main/webapp/imagens/logo.png?raw=true" height="97" alt=""/>
+    	</td>
+    	</tr>
+    	<tr>
+    	<td height="27" colspan="3" bgcolor="#F4F4F4" style="padding:10px 20px;">
+    	<h1 style="padding: 0.5em;margin: 0;"><font size="6" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">E-mail Informativo a OSC</font></h1>
+    	</td>
+    	</tr>
+    	<tr>
+    	<td  colspan="3" bgcolor="#FFFFFF" style="padding:20px;">
+    	<p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Um representante da  <b> '.$nameOSC.'</b> se cadastrou no Mapa das OSCs. Ele está habilitado a inserir dados na página individual da organização.</font> </p>
+    	<p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Abaixo seguem os dados do cadastro. Caso não esteja de acordo que este nome seja o representante da OSC, por favor, nos comunique pelo seguinte e-mail:<b>mapaosc@ipea.gov.br</b>.</font> </p>
+    	<br/>
+    	<p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif"><strong>Dados do Representante:</strong></font></p>
+    	<p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Nome:  '.$user.' </font></p>
+    	<p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">CPF:  "Cpf do Usuário" </font></p>
+    	<p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">E-mail: "Email do Usuario" </font></p>
+    	</td>
+    	</tr>
+    	<tr>
+    	<td width="auto"></td>
+    	<td valign="middle" align="right" style="padding:20px;">
+    	<img src="https://github.com/Plataformas-Cidadania/mapaosc/blob/master/src/main/webapp/imagens/loading.png?raw=true" height="71" width="71" alt=""/>
+    	</td>
+    	<td width="420" bgcolor="#FFFFFF" valign="middle" style="padding: 20px 0;">
+    	<p style="text-align: justify; margin: 0;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Agradecemos pelo contato,</font> </p>
+    	<p style="text-align: justify; margin: 0;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Equipe do Mapa das OSCs</font> </p>
     	<p style="text-align: justify; margin: 0;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif"><a href="https://mapaosc.ipea.gov.br">Mapa das OSCs</a> - '.$this->date().'.</font> </p>
     	</td>
     	</tr>
