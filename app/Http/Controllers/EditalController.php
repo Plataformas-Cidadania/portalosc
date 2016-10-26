48b31e2567eca5ac1a45536d1d7381604fb9ba89
B222412611 <B222412611@ERJ5098.ipea.gov.br>
2016-10-26 16:24:19 -0200

    MOSC-827

--- app/Http/Controllers/EditalController.php
+++ app/Http/Controllers/EditalController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Dao\AdminDao;
+use App\Dao\EditalDao;
 
 class EditalController extends Controller
 {
