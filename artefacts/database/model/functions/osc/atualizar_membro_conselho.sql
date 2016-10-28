ec0ab36fc2cbfae1e85fe44e213164624a8b6ae7
Denise <Denise@192.168.1.5>
2016-10-28 17:55:58 -0200

    Issue: MOSC-602 Subject: Implementação da página de edição da OSC Detail:

---
+++ artefacts/database/model/functions/osc/atualizar_membro_conselho.sql
@@ -0,0 +1,22 @@
+DOP FUNCTION IF EXISTS portal.atualizar_membro_conselho(id INTEGER, idconselheiro INTEGER, nomeconselheiro TEXT, ftnomeconselheiro TEXT);
+
+CREATE OR REPLACE FUNCTION portal.atualizar_membro_conselho(id INTEGER, idconselheiro INTEGER, nomeconselheiro TEXT, ftnomeconselheiro TEXT)
+ RETURNS TABLE(
+	mensagem TEXT
+)AS $$
+
+BEGIN 
+	UPDATE 
+		osc.tb_conselho_fiscal
+	SET 
+		id_osc = id,
+		tx_nome_conselheiro = nomeconselheiro, 
+		ft_nome_conselheiro = ftnomeconselheiro
+		 
+	WHERE 
+		id_conselheiro = idconselheiro; 
+
+	mensagem := 'Membro do conselho atualizado';
+	RETURN NEXT;
+END; 
+$$ LANGUAGE 'plpgsql';
