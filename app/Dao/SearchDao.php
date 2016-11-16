6fad72c81f2779e193881f6d702845ff3efa719e
Raul Sena Ferreira <raulsenaferreira@gmail.com>
2016-11-16 12:43:35 -0200

    otimizando passagem do json pra lista

--- app/Dao/SearchDao.php
+++ app/Dao/SearchDao.php
@@ -42,6 +42,19 @@ class SearchDao extends Dao
 		return $json;
 	}
 
+	private function configResultLista($result){
+		$json = [[]];
+
+		for ($i = 0; $i<count($result); $i++) {
+				$json[$result[$i]->id_osc][0] = $result[$i]->tx_nome_osc;
+				$json[$result[$i]->id_osc][1] = $result[$i]->cd_identificador_osc;
+				$json[$result[$i]->id_osc][2] = $result[$i]->tx_natureza_juridica_osc;
+				$json[$result[$i]->id_osc][3] = $result[$i]->tx_endereco_osc;
+				//$json[$result[$i]->id_osc][4] = $result[$i]->imagemOsc;
+		}
+
+		return $json;
+	}
     public function searchOsc($type_search, $type_result, $param = null)
     {
     	$queries = array();
@@ -55,19 +68,22 @@ class SearchDao extends Dao
     		$queries = $this->queriesGeo;
     	}
 
-        if(array_key_exists($type_search, $queries)){
-            $query_info = $queries[$type_search];
-            $query = $query_info[0];
-            $unique = $query_info[1];
+      if(array_key_exists($type_search, $queries)){
+          $query_info = $queries[$type_search];
+          $query = $query_info[0];
+          $unique = $query_info[1];
 
-            $result = $this->executeQuery($query, $unique, $param);
-        }else{
-            $result = null;
-        }
+          $result = $this->executeQuery($query, $unique, $param);
+      }else{
+          $result = null;
+      }
 
-		if($type_result == 'geo'){
-			$result = $this->configResultGeo($result);
-		}
+			if($type_result == 'lista'){
+				$result = $this->configResultLista($result);
+			}
+			if($type_result == 'geo'){
+				$result = $this->configResultGeo($result);
+			}
 
         return $result;
     }
