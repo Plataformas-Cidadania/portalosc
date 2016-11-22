bb528d19d2033cc0009bd6ee1116eb968e63e86d
Denise <Denise@192.168.1.100>
2016-11-22 14:00:47 -0200

    Issue: MOSC-602 Subject: Implementação da página de edição da OSC Detail:

--- artefacts/database/model/functions/osc/inserir_parceira_projeto.sql
+++ artefacts/database/model/functions/osc/inserir_parceira_projeto.sql
@@ -1,14 +1,14 @@
 DROP FUNCTION IF EXISTS portal.inserir_parceira_projeto(idprojeto INTEGER, idosc INTEGER, ftoscparceiraprojeto TEXT);
 
-CREATE OR REPLACE FUNCTION portal.inserir_parceira_projeto(idprojeto INTEGER, idosc INTEGER, ftoscparceiraprojeto TEXT)
+CREATE OR REPLACE FUNCTION portal.inserir_parceira_projeto(idprojeto INTEGER, idosc INTEGER, ftoscparceiraprojeto TEXT, oficial BOOLEAN)
  RETURNS BOOLEAN AS $$
 
 DECLARE
 	status BOOLEAN;	
 
 BEGIN
-	INSERT INTO osc.tb_osc_parceira_projeto (id_projeto, id_osc, ft_osc_parceira_projeto) 
-	VALUES(idprojeto, idosc, ftoscparceiraprojeto);
+	INSERT INTO osc.tb_osc_parceira_projeto (id_projeto, id_osc, ft_osc_parceira_projeto, bo_oficial) 
+	VALUES(idprojeto, idosc, ftoscparceiraprojeto, oficial);
 
 	status := true;
 	RETURN status;
--- artefacts/database/model/functions/osc/inserir_participacao_social_conferencia.sql
+++ artefacts/database/model/functions/osc/inserir_participacao_social_conferencia.sql
@@ -1,14 +1,14 @@
 DROP FUNCTION IF EXISTS portal.inserir_participacao_social_conferencia(id INTEGER, cdconferencia INTEGER, ftconferencia TEXT, dtanorealizacao DATE, ftanorealizacao TEXT, cdformaparticipacaoconferencia INTEGER, ftformaparticipacaoconferencia TEXT);
 
-CREATE OR REPLACE FUNCTION portal.inserir_participacao_social_conferencia(id INTEGER, cdconferencia INTEGER, ftconferencia TEXT, dtanorealizacao DATE, ftanorealizacao TEXT, cdformaparticipacaoconferencia INTEGER, ftformaparticipacaoconferencia TEXT)
+CREATE OR REPLACE FUNCTION portal.inserir_participacao_social_conferencia(id INTEGER, cdconferencia INTEGER, ftconferencia TEXT, dtanorealizacao DATE, ftanorealizacao TEXT, cdformaparticipacaoconferencia INTEGER, ftformaparticipacaoconferencia TEXT, oficial BOOLEAN)
  RETURNS BOOLEAN AS $$
 
 DECLARE
 	status BOOLEAN;	
 
 BEGIN
-	INSERT INTO osc.tb_participacao_social_conferencia (cd_conferencia, ft_conferencia, id_osc, dt_ano_realizacao, ft_ano_realizacao, cd_forma_participacao_conferencia, ft_forma_participacao_conferencia) 
-	VALUES (cdconferencia, ftconferencia, id, dtanorealizacao, ftanorealizacao, cdformaparticipacaoconferencia, ftformaparticipacaoconferencia);
+	INSERT INTO osc.tb_participacao_social_conferencia (cd_conferencia, ft_conferencia, id_osc, dt_ano_realizacao, ft_ano_realizacao, cd_forma_participacao_conferencia, ft_forma_participacao_conferencia, bo_oficial) 
+	VALUES (cdconferencia, ftconferencia, id, dtanorealizacao, ftanorealizacao, cdformaparticipacaoconferencia, ftformaparticipacaoconferencia, oficial);
 
 	status := true;
 	RETURN status;
