835987828a02646da860f2263c41486868cefadc
EricFerreiraS <eric_santos6@hotmail.com>
2016-11-24 17:54:17 -0200

    Atualizacao de view

--- artefacts/database/model/views/vw_osc_certificado.sql
+++ artefacts/database/model/views/vw_osc_certificado.sql
@@ -1,4 +1,4 @@
--- object: portal.vw_osc_certificado | type: MATERIALIZED VIEW --
+﻿-- object: portal.vw_osc_certificado | type: MATERIALIZED VIEW --
 DROP MATERIALIZED VIEW IF EXISTS portal.vw_osc_certificado CASCADE;
 CREATE MATERIALIZED VIEW portal.vw_osc_certificado
 AS
@@ -10,7 +10,7 @@ SELECT
 	tb_certificado.cd_certificado,
 	(SELECT tx_nome_certificado FROM syst.dc_certificado WHERE cd_certificado = tb_certificado.cd_certificado) AS tx_nome_certificado,
 	TO_CHAR(tb_certificado.dt_inicio_certificado, 'DD-MM-YYYY') AS dt_inicio_certificado,
-	tb_certificado.dt_fim_certificado,
+	TO_CHAR(tb_certificado.dt_fim_certificado, 'DD-MM-YYYY') AS dt_fim_certificado,
 	tb_certificado.ft_certificado
 FROM osc.tb_osc
 INNER JOIN osc.tb_certificado ON tb_osc.id_osc = tb_certificado.id_osc
