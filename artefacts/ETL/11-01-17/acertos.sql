bd402305c2e8a24526d8452977f21922977d4007
EricFerreiraS <eric_santos6@hotmail.com>
2017-01-11 19:20:57 -0200

    MOSC-934 acerto do CEBAS/MS

---
+++ artefacts/ETL/11-01-17/acertos.sql
@@ -0,0 +1,4 @@
+﻿UPDATE osc.tb_projeto SET dt_data_inicio_projeto = null where dt_data_inicio_projeto = '0001-01-01 BC';
+UPDATE osc.tb_projeto SET dt_data_fim_projeto = null where dt_data_fim_projeto = '0001-01-01 BC';
+DELETE FROM osc.tb_certificado WHERE ft_certificado = 'MS/SUS';
+
