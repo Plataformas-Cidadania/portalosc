3b91d3e2ceec40f950f4209afeb186a929e9bd85
vagnerpraia <vagnerpraia@gmail.com>
2017-01-23 12:53:50 -0200

    Adição da função de busca por CNPJ

---
+++ artefacts/database/model/functions/busca/buscar_osc_cnpj.sql
@@ -0,0 +1,35 @@
+﻿DROP FUNCTION portal.buscar_osc_cnpj(text, integer, integer);
+
+CREATE OR REPLACE FUNCTION portal.buscar_osc_cnpj(param text, limit_result integer, offset_result integer) RETURNS TABLE(
+	id_osc INTEGER, 
+	tx_nome_osc TEXT
+) AS $$ 
+
+DECLARE 
+	query_limit TEXT; 
+
+BEGIN	
+	IF offset_result > 0 THEN 
+		query_limit := 'LIMIT ' || limit_result || ' OFFSET ' || offset_result || ';'; 
+	ELSIF limit_result > 0 THEN 
+		query_limit := 'LIMIT ' || limit_result || ';'; 
+	ELSE 
+		query_limit := ';'; 
+	END IF; 
+	
+	RETURN QUERY 
+		EXECUTE 
+			'SELECT vw_busca_osc.id_osc, vw_busca_osc.tx_razao_social_osc as tx_nome_osc
+			FROM portal.vw_busca_osc
+			WHERE document @@ to_tsquery(''portuguese_unaccent'', LTRIM(''' || param::TEXT || ''', ''0''))
+			AND (
+			   similarity(vw_busca_osc.cd_identificador_osc::TEXT, LTRIM(''' || param::TEXT || ''', ''0'')) > 0.8 OR
+			   similarity(vw_busca_osc.tx_razao_social_osc::TEXT, ''' || param::TEXT || ''') > 0.2 OR
+			   similarity(vw_busca_osc.tx_nome_fantasia_osc::TEXT, ''' || param::TEXT || ''') > 0.2
+			)
+			ORDER BY GREATEST(
+				similarity(vw_busca_osc.cd_identificador_osc::TEXT, LTRIM(''' || param::TEXT || ''', ''0'')),
+				similarity(vw_busca_osc.tx_razao_social_osc::TEXT, ''' || param::TEXT || '''),
+				similarity(vw_busca_osc.tx_nome_fantasia_osc::TEXT, ''' || param::TEXT || ''')
+			) DESC ' || query_limit; 
+END;
