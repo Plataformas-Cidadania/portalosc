48a26064a521bdf5352ebd7938bf0bef552b42cf
vagnerpraia <vagnerpraia@gmail.com>
2017-03-08 17:48:51 -0300

    Atualização da view e function dos recursos de projetos

--- artefacts/database/functions/osc/obter_recursos_projeto.sql
+++ artefacts/database/functions/osc/obter_recursos_projeto.sql
@@ -2,11 +2,6 @@ DROP FUNCTION IF EXISTS portal.obter_recursos_projeto(param TEXT);
 
 CREATE OR REPLACE FUNCTION portal.obter_recursos_projeto(param TEXT) RETURNS TABLE (
 	nr_valor_total DOUBLE PRECISION, 
-	nr_valor_federal DOUBLE PRECISION, 
-	nr_valor_estadual DOUBLE PRECISION, 
-	nr_valor_municipal DOUBLE PRECISION, 
-	nr_valor_privado DOUBLE PRECISION, 
-	nr_valor_proprio DOUBLE PRECISION, 
 	tx_link_relatorio_auditoria TEXT, 
 	ft_link_relatorio_auditoria TEXT, 
 	tx_link_demonstracao_contabil TEXT, 
@@ -16,11 +11,6 @@ BEGIN
 	RETURN QUERY 
 		SELECT 
 			vw_osc_recursos_projeto.nr_valor_total, 
-			vw_osc_recursos_projeto.nr_valor_federal, 
-			vw_osc_recursos_projeto.nr_valor_estadual, 
-			vw_osc_recursos_projeto.nr_valor_municipal, 
-			vw_osc_recursos_projeto.nr_valor_privado, 
-			vw_osc_recursos_projeto.nr_valor_proprio, 
 			vw_osc_recursos_projeto.tx_link_relatorio_auditoria, 
 			vw_osc_recursos_projeto.ft_link_relatorio_auditoria, 
 			vw_osc_recursos_projeto.tx_link_demonstracao_contabil, 
--- artefacts/database/views/vw_osc_recursos_projeto.sql
+++ artefacts/database/views/vw_osc_recursos_projeto.sql
@@ -7,31 +7,6 @@ SELECT
 	tb_osc.id_osc,
 	tb_osc.tx_apelido_osc,
 	(SELECT sum(tb_projeto.nr_valor_total_projeto) FROM osc.tb_projeto WHERE tb_projeto.id_osc = tb_osc.id_osc) AS nr_valor_total,
-	(SELECT sum(projeto.nr_valor_total_projeto)	FROM osc.tb_projeto AS projeto INNER JOIN osc.tb_fonte_recursos_projeto AS recursos	ON projeto.id_projeto = recursos.id_projeto	WHERE (
-		SELECT tx_nome_fonte_recursos_projeto
-		FROM syst.dc_fonte_recursos_projeto
-		WHERE cd_fonte_recursos_projeto = recursos.cd_fonte_recursos_projeto
-	) = 'Federal') AS nr_valor_federal,
-	(SELECT sum(projeto.nr_valor_total_projeto)	FROM osc.tb_projeto AS projeto INNER JOIN osc.tb_fonte_recursos_projeto AS recursos	ON projeto.id_projeto = recursos.id_projeto	WHERE (
-		SELECT tx_nome_fonte_recursos_projeto
-		FROM syst.dc_fonte_recursos_projeto
-		WHERE cd_fonte_recursos_projeto = recursos.cd_fonte_recursos_projeto
-	) = 'Estadual') AS nr_valor_estadual,
-	(SELECT sum(projeto.nr_valor_total_projeto)	FROM osc.tb_projeto AS projeto INNER JOIN osc.tb_fonte_recursos_projeto AS recursos	ON projeto.id_projeto = recursos.id_projeto	WHERE (
-		SELECT tx_nome_fonte_recursos_projeto
-		FROM syst.dc_fonte_recursos_projeto
-		WHERE cd_fonte_recursos_projeto = recursos.cd_fonte_recursos_projeto
-	) = 'Municipal') AS nr_valor_municipal,
-	(SELECT sum(projeto.nr_valor_total_projeto)	FROM osc.tb_projeto AS projeto INNER JOIN osc.tb_fonte_recursos_projeto AS recursos	ON projeto.id_projeto = recursos.id_projeto	WHERE (
-		SELECT cd_origem_fonte_recursos_projeto
-		FROM syst.dc_fonte_recursos_projeto
-		WHERE cd_fonte_recursos_projeto = recursos.cd_fonte_recursos_projeto
-	) = 3) AS nr_valor_privado,
-	(SELECT sum(projeto.nr_valor_total_projeto)	FROM osc.tb_projeto AS projeto INNER JOIN osc.tb_fonte_recursos_projeto AS recursos	ON projeto.id_projeto = recursos.id_projeto	WHERE (
-		SELECT cd_origem_fonte_recursos_projeto
-		FROM syst.dc_fonte_recursos_projeto
-		WHERE cd_fonte_recursos_projeto = recursos.cd_fonte_recursos_projeto
-	) = 4) AS nr_valor_proprio,
 	tb_dados_gerais.tx_link_relatorio_auditoria,
 	tb_dados_gerais.ft_link_relatorio_auditoria,
 	tb_dados_gerais.tx_link_demonstracao_contabil,
