d0161cfb99c01d6cb8ef7259637e66477b3d8f02
vagnerpraia <vagnerpraia@192.168.58.2>
2017-03-15 23:27:21 -0300

    Alteração de código para debug

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -48,13 +48,12 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-        return parent::render($request, $e);
-		/*
+        //return parent::render($request, $e);
+		
         $this->report($e);
 
     	$content_response = ['msg' => 'Ocorreu um erro'];
     	$http_code = 500;
         return Response($content_response, $http_code);
-        */
     }
 }
