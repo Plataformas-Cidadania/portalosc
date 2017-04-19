4a3a003ba5c991c4b535cdd4f3258b515a0cc4ab
vagnerpraia <vagnerpraia@gmail.com>
2017-04-19 16:39:05 -0300

    Atualização para adição da periodicidade de reunião por menu

---
+++ artefacts/database/alter/alter_tb_participacao_social_conselho.sql
@@ -0,0 +1,10 @@
+﻿DROP MATERIALIZED VIEW IF EXISTS portal.vw_osc_participacao_social_conselho;
+
+ALTER TABLE osc.tb_participacao_social_conselho DROP COLUMN tx_periodicidade_reuniao RESTRICT;
+
+ALTER TABLE osc.tb_participacao_social_conselho ADD cd_periodicidade_reuniao_conselho INTEGER UNIQUE;
+
+ALTER TABLE osc.tb_participacao_social_conselho 
+ADD CONSTRAINT fk_cd_periodicidade_reuniao_conselho FOREIGN KEY (cd_periodicidade_reuniao_conselho) 
+REFERENCES syst.dc_periodicidade_reuniao_conselho (cd_periodicidade_reuniao_conselho) MATCH FULL 
+ON UPDATE NO ACTION ON DELETE NO ACTION;
