139ce9c2d16c211f110fc2b1218529b9a567d928
Heraldo Borges <heraldoborges@gmail.com>
2017-04-20 19:53:23 -0300

    Issue: MOSC-479

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -59,7 +59,7 @@ class EmailController extends Controller {
 		<tbody>
 		<tr>
 		<td colspan="3" style="padding:20px;">
-		<img src="https://github.com/Plataformas-Cidadania/mapaosc/blob/master/src/main/webapp/imagens/logo.png?raw=true" height="97" alt=""/>
+		<img src="http://mapaosc.ipea.gov.br/img/logo.png" height="97" alt=""/>
 		</td>
 		</tr>
 		<tr>
@@ -104,7 +104,7 @@ class EmailController extends Controller {
     	<tbody>
     	<tr>
     	<td colspan="3" style="padding:20px;">
-    	<img src="https://github.com/Plataformas-Cidadania/mapaosc/blob/master/src/main/webapp/imagens/logo.png?raw=true" height="97" alt=""/>
+    	<img src="http://mapaosc.ipea.gov.br/img/logo.png" height="97" alt=""/>
     	</td>
     	</tr>
     	<tr>
@@ -152,7 +152,7 @@ class EmailController extends Controller {
     	<tbody>
     	<tr>
     	<td colspan="3" style="padding:20px;">
-    	<img src="https://github.com/Plataformas-Cidadania/mapaosc/blob/master/src/main/webapp/imagens/logo.png?raw=true" height="97" alt=""/>
+    	<img src="http://mapaosc.ipea.gov.br/img/logo.png" height="97" alt=""/>
     	</td>
     	</tr>
     	<tr>
@@ -256,7 +256,7 @@ class EmailController extends Controller {
     	<tbody>
     	<tr>
     	<td colspan="3" style="padding:20px;">
-    	<img src="https://github.com/Plataformas-Cidadania/mapaosc/blob/master/src/main/webapp/imagens/logo.png?raw=true" height="97" alt=""/>
+    	<img src="http://mapaosc.ipea.gov.br/img/logo.png" height="97" alt=""/>
     	</td>
     	</tr>
     	<tr>
@@ -303,7 +303,7 @@ class EmailController extends Controller {
     	<tbody>
     	<tr>
     	<td colspan="3" style="padding:20px;">
-    	<img src="https://github.com/Plataformas-Cidadania/mapaosc/blob/master/src/main/webapp/imagens/logo.png?raw=true" height="97" alt=""/>
+    	<img src="http://mapaosc.ipea.gov.br/img/logo.png" height="97" alt=""/>
     	</td>
     	</tr>
     	<tr>
