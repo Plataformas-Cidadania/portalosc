f00ac898c59f43bbcba0747c87cf0db7e6728be4
vagnerpraia <vagnerpraia@gmail.com>
2017-04-26 18:18:35 -0300

    Acerto função

--- artefacts/database/functions/busca/buscar_osc.sql
+++ artefacts/database/functions/busca/buscar_osc.sql
@@ -7,7 +7,7 @@ CREATE OR REPLACE FUNCTION portal.buscar_osc(param TEXT, limit_result INTEGER, o
 DECLARE 
 	query_limit TEXT; 
 
-BEGIN	
+BEGIN 
 	IF offset_result > 0 THEN 
 		query_limit := 'LIMIT ' || limit_result || ' OFFSET ' || offset_result || ';'; 
 	ELSIF limit_result > 0 THEN 
@@ -18,11 +18,10 @@ BEGIN
 	
 	param := LOWER(param); 
 	
-	
 	RETURN QUERY 
 		EXECUTE 
-			'SELECT vw_busca_osc.id_osc
-			FROM osc.vw_busca_osc
+			'SELECT vw_busca_osc.id_osc 
+			FROM osc.vw_busca_osc 
 			WHERE 
 			(
 				similarity(vw_busca_osc.cd_identificador_osc::TEXT, LTRIM(''' || param::TEXT || ''', ''0'')) >= 0.25 
@@ -30,15 +29,15 @@ BEGIN
 			) 
 			OR 
 			(
-				document @@ to_tsquery(''portuguese_unaccent'', ''' || param::TEXT || ''')
+				document @@ to_tsquery(''portuguese_unaccent'', ''' || param::TEXT || ''') 
 				AND (
-					similarity(vw_busca_osc.tx_razao_social_osc::TEXT, ''' || param::TEXT || ''') > ' || similarity_result::DOUBLE PRECISION || ' OR
+					similarity(vw_busca_osc.tx_razao_social_osc::TEXT, ''' || param::TEXT || ''') > ' || similarity_result::DOUBLE PRECISION || ' OR 
 					similarity(vw_busca_osc.tx_nome_fantasia_osc::TEXT, ''' || param::TEXT || ''') > ' || similarity_result::DOUBLE PRECISION || '
 				)
 			)
 			ORDER BY GREATEST(
-				similarity(vw_busca_osc.cd_identificador_osc::TEXT, LTRIM(''' || param::TEXT || ''', ''0'')),
-				similarity(vw_busca_osc.tx_razao_social_osc::TEXT, ''' || param::TEXT || '''),
+				similarity(vw_busca_osc.cd_identificador_osc::TEXT, LTRIM(''' || param::TEXT || ''', ''0'')), 
+				similarity(vw_busca_osc.tx_razao_social_osc::TEXT, ''' || param::TEXT || '''), 
 				similarity(vw_busca_osc.tx_nome_fantasia_osc::TEXT, ''' || param::TEXT || ''')
 			) DESC ' || query_limit; 
 END; 
