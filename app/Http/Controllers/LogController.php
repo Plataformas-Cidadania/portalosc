4018d4425671641bb79307d425447949b57e290f
EricFerreiraS <eric_santos6@hotmail.com>
2017-05-11 15:14:34 -0300

    Correção de bugs

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -11,14 +11,14 @@ class LogController extends Controller
 	{
 		$this->log = new LogDao();
 	}
-    
+
     public function saveLog($table_name, $id_osc, $id_user, $tx_dado_anterior, $tx_dado_posterior){
     	if($tx_dado_posterior){
     		$tx_dado_anterior = '{' . rtrim($tx_dado_anterior, ',') . '}';
     		$tx_dado_posterior = '{' . rtrim($tx_dado_posterior, ',') . '}';
-    		
+
     		$params = [$table_name, $id_osc, $id_user, date("Y-m-d H:i:s"), $tx_dado_anterior, $tx_dado_posterior];
-    		$resultDaoLog = $this->log->insertLog2($params);
+    		//$resultDaoLog = $this->log->insertLog2($params);
     	}
     }
 }
--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -8,33 +8,32 @@ use App\Dao\SearchDao;
 class SearchController extends Controller
 {
 	private $dao;
-	
+
 	public function __construct()
 	{
 		$this->dao = new SearchDao();
 	}
-	
+
     public function getSearchList($type_result, $limit = 0, $offset = 0)
     {
 		$param = [$limit, $offset];
-		
+
 		$resultDao = $this->dao->searchList($type_result, $param);
 		$this->configResponse($resultDao);
         return $this->response();
     }
-	
+
     public function getSearch($type_search, $type_result, $param, $limit = 0, $offset = 0, $similarity = '05')
     {
     	$param = trim($param);
-    	
+
     	$similarity = '0.' . $similarity;
-    	
-    	if($type_search == 'cnpj'){
+    	if($type_search == 'cnpj' || $type_search == 'municipio' || $type_search == 'estado'){
     		$param = [$param, $limit, $offset];
     	}else{
     		$param = [$param, $limit, $offset, $similarity];
     	}
-    	
+
     	$resultDao = $this->dao->search($type_search, $type_result, $param);
     	$this->configResponse($resultDao);
     	return $this->response();
