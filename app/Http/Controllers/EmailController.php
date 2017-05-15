211f6df749c26c4d07d9169cd0c9e56f7c829987
Heraldo Borges <heraldoborges@gmail.com>
2017-05-15 15:16:25 -0300

    Update EmailController.php
    MOSC-1191 Rotina de envio de e-mail para recadastro de senha

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -185,6 +185,57 @@ class EmailController extends Controller {
     	</body>
     	</html>';
     }
+	
+	
+public function changePasswordUser($name, $token)
+    {
+      $baseurl = env('BASE_URL');
+
+      return
+      '<html>
+      <head>
+      <title>Novo Mapa das Organizações da Sociedade Civil</title>
+      </head>
+      <body bgcolor="#FFFFFF" style="margin: 0 auto; font-size: 16px;">
+      <table id="Table_01" border="0" align="center" cellpadding="0" cellspacing="0" style="border: 2px solid #f4f4f4; min-width:300px; width:100%; max-width:700px; margin:20px auto;">
+      <tbody>
+      <tr>
+      <td colspan="3" style="padding:20px;">
+      <img src="http://mapaosc.ipea.gov.br/img/logo.png" height="97" alt=""/>
+      </td>
+      </tr>
+      <tr>
+      <td height="27" colspan="3" bgcolor="#F4F4F4" style="padding:10px 20px;">
+      <h1 style="padding: 0.5em;margin: 0;"><font size="6" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Novo Mapa das Organizações da
+      Sociedade Civil</font></h1>
+      </td>
+      </tr>
+      <tr>
+      <td  colspan="3" bgcolor="#FFFFFF" style="padding:20px;">
+      <p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Olá, '.$name.'!</font> </p>
+      <p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">O Mapa das Organizações da Sociedade Civil está de cara nova. </font> </p>
+      <p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Para continuar com a acesso como representante de organização, é necessário que cadastre uma nova senha. Para isso, clique no link* abaixo:</font> </p>
+      <p style="text-indent: 2.5em;text-align: justify;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif"><a href="'.$baseurl.'/alterar-senha.html?token='.$token.'">'.$baseurl.'/alterar-senha.html?token='.$token.'</a></font> </p>
+      <p style="text-indent: 2.5em;text-align: justify;"> </p>
+      <p style="text-indent: 2.5em;text-align: justify;"> <font size="2" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">*Este link expira em 24 horas.</font> </p>
+      </td>
+      </tr>
+      <tr>
+      <td width="auto"></td>
+      <td valign="middle" align="right" style="padding:20px;">
+      <img src="https://github.com/Plataformas-Cidadania/mapaosc/blob/master/src/main/webapp/imagens/loading.png?raw=true" height="71" width="71" alt=""/>
+      </td>
+      <td width="420" bgcolor="#FFFFFF" valign="middle" style="padding: 20px 0;">
+      <p style="text-align: justify; margin: 0;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Agradecemos pelo contato,</font> </p>
+      <p style="text-align: justify; margin: 0;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif">Equipe do Mapa das OSCs</font> </p>
+      <p style="text-align: justify; margin: 0;"> <font size="4" face="Roboto, arial narrow, helvetica condensed, helvetica, arial, sans-serif"><a href="https://mapaosc.ipea.gov.br">Mapa das OSCs</a> - '.$this->date().'.</font> </p>
+      </td>
+      </tr>
+      </tbody>
+      </table>
+      </body>
+      </html>'
+    }
 
     public function informationOSC($user, $nameOSC)
     {
