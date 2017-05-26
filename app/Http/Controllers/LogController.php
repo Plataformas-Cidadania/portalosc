10034abde890fc039b56f61cb5ef7de8c23c84a4
vagnerpraia <vagnerpraia@gmail.com>
2017-05-26 18:33:30 -0300

    Ajuste na conversão para json na gravação do log

--- app/Http/Controllers/LogController.php
+++ app/Http/Controllers/LogController.php
@@ -13,12 +13,12 @@ class LogController extends Controller
 	}
 	
 	public function saveLog($table_name, $id_osc, $id_user, $tx_dado_anterior, $tx_dado_posterior){
-		$tx_dado_anterior = str_replace('\\', '\\\\', $tx_dado_anterior);
-		$tx_dado_posterior = str_replace('\\', '\\\\', $tx_dado_posterior);
-		
 		if($tx_dado_anterior != '' && $tx_dado_posterior != ''){
     		if($tx_dado_anterior != null && substr($tx_dado_anterior, 0, 1) != '{') $tx_dado_anterior = '{' . rtrim($tx_dado_anterior, ',') . '}';
     		if($tx_dado_posterior != null && substr($tx_dado_posterior, 0, 1) != '{') $tx_dado_posterior = '{' . rtrim($tx_dado_posterior, ',') . '}';
+    		
+    		$tx_dado_anterior = json_encode($tx_dado_anterior);
+    		$tx_dado_posterior = json_encode($tx_dado_posterior);
     		
 	   		$params = [$table_name, $id_osc, $id_user, date("Y-m-d H:i:s"), $tx_dado_anterior, $tx_dado_posterior];
 	   		$resultDaoLog = $this->log->insertLog($params);
