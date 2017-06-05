4c6b28311ec4a1556e5fd7dfd9abe31d6ac390a5
vagnerpraia <vagnerpraia@gmail.com>
2017-06-05 18:35:03 -0300

    Acerto da busca por região

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -8,34 +8,36 @@ use App\Dao\SearchDao;
 class SearchController extends Controller
 {
 	private $dao;
-
+	
 	public function __construct()
 	{
 		$this->dao = new SearchDao();
 	}
-
+	
     public function getSearchList($type_result, $limit = 0, $offset = 0)
     {
 		$param = [$limit, $offset];
-
+		
 		$resultDao = $this->dao->searchList($type_result, $param);
 		$this->configResponse($resultDao);
         return $this->response();
     }
-
+	
     public function getSearch($type_search, $type_result, $param, $limit = 0, $offset = 0, $similarity = '05')
     {
     	$param = trim($param);
-
+		
     	$similarity = '0.' . $similarity;
-    	if($type_search == 'cnpj' || $type_search == 'municipio' || $type_search == 'estado'){
-    		$param = [$param, $limit, $offset];
-    	}else{
+    	
+    	if($type_search == 'osc'){
     		$param = [$param, $limit, $offset, $similarity];
+    	}else{
+    		$param = [$param, $limit, $offset];
     	}
-
+		
     	$resultDao = $this->dao->search($type_search, $type_result, $param);
     	$this->configResponse($resultDao);
+    	
     	return $this->response();
     }
 }
