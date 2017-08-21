b046676de3cbb0315ae7ede2b37302acdefa17b6
vagnerpraia <vagnerpraia@gmail.com>
2017-08-21 16:27:14 -0300

    Issue: MOSC-1332 Subject: Criar adaptação no back-end para a opção "não possui" Detail: Adição da edição de recursos de OSC

--- app/Services/Osc/EditarRecursosOscService.php
+++ app/Services/Osc/EditarRecursosOscService.php
@@ -20,33 +20,120 @@ class EditarRecursosOscService extends Service
 		$flagModel = $this->analisarRequisicao($model);
 		
 		if($flagModel){
-			$requisicao = $model->getRequisicao();
-			
-			$oscDao = new OscDao();
-			$recursosExistentes = $oscDao->obterRecursosOsc($requisicao->id_osc);
+			$recursosExistente = (new OscDao())->obterRecursosOsc($model->getRequisicao()->id_osc);
 			
 			$arrayInsert = array();
 			$arrayUpdate = array();
-			$arrayDelete = $recursosExistentes;
+			$arrayDelete = $recursosExistente;
 			
-			foreach($recursosExistentes as $recursoExistente){
-				foreach($requisicao->fonte_recursos as $recursoRequisicao){
-					
+			$idusuario = $this->requisicao->getusuario()->id_usuario;
+			
+			foreach($model->getRequisicao()->fonte_recursos as $recursosRequisicao){
+				$flagInsert = true;
+				
+				if($recursosRequisicao->nr_valor_recursos_osc){
+					$recursosRequisicao->bo_nao_possui = false;
+				}
+				
+				foreach ($recursosExistente as $keyRecursosBd => $recursosBd) {
+					if($recursosBd->cd_fonte_recursos_osc == $recursosRequisicao->cd_fonte_recursos_osc && $recursosBd->dt_ano_recursos_osc == $recursosRequisicao->dt_ano_recursos_osc){
+						$flagInsert = false;
+						
+						unset($arrayDelete[$keyRecursosBd]);
+						
+						if($recursosBd->nr_valor_recursos_osc != $recursosRequisicao->nr_valor_recursos_osc){
+							$recursosRequisicao->id_recursos_osc = $recursosBd->id_recursos_osc;
+							$recursosRequisicao->id_osc = $model->getRequisicao()->id_osc;
+							$recursosRequisicao->ft_fonte_recursos_osc = 'Representante';
+							$recursosRequisicao->ft_ano_recursos_osc = 'Representante';
+							$recursosRequisicao->ft_valor_recursos_osc = 'Representante';
+							array_push($arrayUpdate, $recursosRequisicao);
+						}
+					}
+				}
+				
+				if($flagInsert){
+					$recursosRequisicao->id_osc = $model->getRequisicao()->id_osc;
+					$recursosRequisicao->ft_fonte_recursos_osc = 'Representante';
+					$recursosRequisicao->ft_ano_recursos_osc = 'Representante';
+					$recursosRequisicao->ft_valor_recursos_osc = 'Representante';
+					array_push($arrayInsert, $recursosRequisicao);
 				}
 			}
 			
+			$recursosDelete = $this->excluirListaRecursosOsc($arrayDelete);
+			$recursosUpdate = $this->atualizarListaRecursosOsc($arrayUpdate);
+			$recursosInsert = $this->inserirListaRecursosOsc($arrayInsert);
 			
+			$mensagensErro = $this->analisarResultadosDao($recursosDelete, $recursosUpdate, $recursosInsert);
 			
-			foreach($requisicao->fonte_recursos as $recurso){		
-				if($recurso->nr_valor_recursos_osc){
-					$recurso->nr_valor_recursos_osc = true;
-				}
+			if($mensagensErro){
+				$this->resposta->prepararResposta(['msg' => 'Recursos de OSC foram atualizados, mas não completamente. Ocorreu algum erro na atualização dos dados.'], 202);
+			}else{
+				$this->resposta->prepararResposta(['msg' => 'Recursos de OSC foram atualizados.'], 200);
 			}
-			
-			/*
-			 $representacao = $oscDao->inserirRecursosOsc($requisicao);
-			 */
 		}
+	}
+	
+	private function analisarResultadosDao($recursosDelete, $recursosUpdate, $recursosInsert)
+	{
+		$mensagensErro = array();
+		
+		foreach($recursosDelete as $resultadoDao){
+			if(!$resultadoDao->flag){
+				array_push($mensagensErro, $resultadoDao);
+			}
+		}
+		
+		foreach($recursosUpdate as $resultadoDao){
+			if(!$resultadoDao->flag){
+				array_push($mensagensErro, $resultadoDao);
+			}
+		}
+		
+		foreach($recursosInsert as $resultadoDao){
+			if(!$resultadoDao->flag){
+				array_push($mensagensErro, $resultadoDao);
+			}
+		}
+		
+		return $mensagensErro;
+	}
+	
+	private function excluirListaRecursosOsc($arrayDelete)
+	{
+		$arrayResultadoDao = array();
+		
+		foreach($arrayDelete as $recursosOsc){
+			$resultadoDao = (new OscDao())->excluirRecursosOsc($recursosOsc->id_recursos_osc);
+			array_push($arrayResultadoDao, $resultadoDao);
+		}
+		
+		return $arrayResultadoDao;
+	}
+	
+	private function atualizarListaRecursosOsc($arrayUpdate)
+	{
+		$arrayResultadoDao = array();
+		
+		foreach($arrayUpdate as $recursosOsc){
+			$resultadoDao = (new OscDao())->atualizarRecursosOsc($recursosOsc);
+			array_push($arrayResultadoDao, $resultadoDao);
+		}
+		
+		return $arrayResultadoDao;
+	}
+	
+	private function inserirListaRecursosOsc($arrayInsert)
+	{
+		$arrayResultadoDao = array();
+		
+		foreach($arrayInsert as $recursosOsc){
+			$resultadoDao = (new OscDao())->inserirRecursosOsc($recursosOsc);
+			array_push($arrayResultadoDao, $resultadoDao);
+		}
+		
+		return $arrayResultadoDao;
 	}
 	
 	private function analisarRequisicao($model)
@@ -67,9 +154,9 @@ class EditarRecursosOscService extends Service
 	private function validarRecurso($recurso)
 	{
 		$contrato = [
-			'cd_fonte_recursos_osc' => ['apelidos' => NomenclaturaAtributoEnum::CD_FONTE_RECURSOS_OSC, 'obrigatorio' => false, 'tipo' => 'integer'],
+			'cd_fonte_recursos_osc' => ['apelidos' => NomenclaturaAtributoEnum::CD_FONTE_RECURSOS_OSC, 'obrigatorio' => true, 'tipo' => 'integer'],
 			'dt_ano_recursos_osc' => ['apelidos' => NomenclaturaAtributoEnum::ANO_RECURSOS_OSC, 'obrigatorio' => true, 'tipo' => 'date'],
-			'nr_valor_recursos_osc' => ['apelidos' => NomenclaturaAtributoEnum::VALOR_RECURSOS_OSC, 'obrigatorio' => true, 'tipo' => 'float'],
+			'nr_valor_recursos_osc' => ['apelidos' => NomenclaturaAtributoEnum::VALOR_RECURSOS_OSC, 'obrigatorio' => false, 'tipo' => 'float'],
 			'bo_nao_possui' => ['apelidos' => NomenclaturaAtributoEnum::NAO_POSSUI, 'obrigatorio' => false, 'tipo' => 'bollean']
 		];
 		
