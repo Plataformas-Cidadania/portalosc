41be6de0d5e8124f73dac754517a053875f5cf7a
vagnerpraia <vagnerpraia@gmail.com>
2018-01-22 16:53:32 -0200

    Atualização de versão no serviço de sobre

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -62,7 +62,7 @@ class Controller extends BaseController
 	{
 	    $sobre = [
 	        'nome' => 'API Mapa OSC',
-	        'versao' => '2.4.0'
+	        'versao' => '2.4.9'
 	    ];
 	    
 	    $this->resposta->prepararResposta($sobre, 200);
