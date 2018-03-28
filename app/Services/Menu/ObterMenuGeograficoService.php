36bbac6864807ba84b7069e36520c1724720f979
vagnerpraia <vagnerpraia@gmail.com>
2018-03-28 20:09:15 -0300

    Acerto dos serviços de menu

--- app/Services/Menu/ObterMenuGeograficoService.php
+++ app/Services/Menu/ObterMenuGeograficoService.php
@@ -35,7 +35,7 @@ class ObterMenuGeograficoService extends BaseService
 		
 		$requisicao = $this->requisicao->getConteudo();
 		
-		$modelo = new Model();
+		$modelo = new BaseModel();
 		$modelo->configurarEstrutura($estrutura);
     	$modelo->configurarRequisicao($requisicao);
 		$modelo->analisarRequisicao();
--- app/Services/Menu/ObterMenuOscService.php
+++ app/Services/Menu/ObterMenuOscService.php
@@ -25,7 +25,7 @@ class ObterMenuOscService extends BaseService
 		
 		$requisicao = $this->requisicao->getConteudo();
 		
-		$modelo = new Model();
+		$modelo = new BaseModel();
 		$modelo->configurarEstrutura($estrutura);
     	$modelo->configurarRequisicao($requisicao);
 		$modelo->analisarRequisicao();
