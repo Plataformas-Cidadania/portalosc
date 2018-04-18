06fae15736a71c195d8578867277796afa0add8e
vagnerpraia <vagnerpraia@gmail.com>
2018-04-18 19:43:14 -0300

    Refatoração do obter projeto

--- app/Dao/OscDao.php
+++ app/Dao/OscDao.php
@@ -76,9 +76,10 @@ class OscDao extends DaoPostgres{
     	}
 
     	if($with_project){
-	    	$result_query = (new ProjetoDao)->obterProjetos($modelo);
+			$result_query = (new ProjetoDao)->obterProjetos($modelo);
 	    	if($result_query){
-	    		$result = array_merge($result, ["projeto" => $result_query]);
+				$objeto = json_decode($result_query->resultado);
+	    		$result = array_merge($result, ["projeto" => $objeto]);
 	    	}
     	}else{
 			$result_query = $this->getComponentOsc("projeto_abreviado", $param);
@@ -261,147 +262,6 @@ class OscDao extends DaoPostgres{
         }else{
             return $result;
         }
-    }
-	
-    private function getProjeto($param){
-    	$result = array();
-    	$query = "SELECT * FROM portal.obter_osc_projeto_id_osc(?::TEXT);";
-        $result_query = $this->executarQuery($query, false, [$param]);
-		
-		if($result_query){
-			$result_partial = array();
-			
-	        foreach($result_query as $key => $projeto){
-				$result_projeto = array();
-				
-				foreach($projeto as $key_projeto => $value_projeto){
-		        	$result_projeto = array_merge($result_projeto, [$key_projeto => $value_projeto]);
-				}
-				
-		        $query = "SELECT id_fonte_recursos_projeto, cd_origem_fonte_recursos_projeto, tx_nome_origem_fonte_recursos_projeto AS tx_nome_valor, ft_fonte_recursos_projeto FROM portal.obter_osc_fonte_recursos_projeto(?::INTEGER);";
-		        $result_query_partial = $this->executarQuery($query, false, [$projeto->id_projeto]);
-		        if($result_query_partial){
-		        	$array_partial = array();
-		            foreach($result_query_partial as $key_recursos_projeto => $value_recursos_projeto){
-		        		$array_partial = array_merge($array_partial, [$key_recursos_projeto => $value_recursos_projeto]);
-		        	}
-		            $result_projeto = array_merge($result_projeto, ["fonte_recursos" => $array_partial]);
-				}else{
-					$result_projeto = array_merge($result_projeto, ["fonte_recursos" => null]);
-				}
-				
-		        $query = "SELECT id_tipo_parceria_projeto, cd_tipo_parceria_projeto, tx_nome_tipo_parceria AS tx_nome_valor, ft_tipo_parceria_projeto FROM portal.vw_osc_tipo_parceria_projeto WHERE id_projeto = ?::INTEGER;";
-		        $result_query_partial = $this->executarQuery($query, false, [$projeto->id_projeto]);
-		        if($result_query_partial){
-		        	$array_partial = array();
-		            foreach($result_query_partial as $key_recursos_projeto => $value_recursos_projeto){
-		        		$array_partial = array_merge($array_partial, [$key_recursos_projeto => $value_recursos_projeto]);
-		        	}
-		            $result_projeto = array_merge($result_projeto, ["tipo_parceria" => $array_partial]);
-				}else{
-					$result_projeto = array_merge($result_projeto, ["tipo_parceria" => null]);
-				}
-				
-				$query = "SELECT * FROM portal.obter_osc_publico_beneficiado_projeto(?::INTEGER);";
-				$result_query_partial = $this->executarQuery($query, false, [$projeto->id_projeto]);
-				if($result_query_partial){
-					$array_partial = array();
-					foreach($result_query_partial as $key_recursos_projeto => $value_recursos_projeto){
-						$array_partial = array_merge($array_partial, [$key_recursos_projeto => $value_recursos_projeto]);
-					}
-					$result_projeto = array_merge($result_projeto, ["publico_beneficiado" => $array_partial]);
-				}
-				
-				$query = "SELECT id_financiador_projeto, tx_nome_financiador, ft_nome_financiador FROM portal.vw_osc_financiador_projeto WHERE id_projeto = ?::INTEGER;";
-				$result_query_partial = $this->executarQuery($query, false, [$projeto->id_projeto]);
-				if($result_query_partial){
-					$array_partial = array();
-					foreach($result_query_partial as $key_recursos_projeto => $value_recursos_projeto){
-						$array_partial = array_merge($array_partial, [$key_recursos_projeto => $value_recursos_projeto]);
-					}
-					$result_projeto = array_merge($result_projeto, ["financiador_projeto" => $array_partial]);
-				}
-				
-				$query = "SELECT * FROM portal.obter_osc_area_atuacao_projeto(?::INTEGER);";
-				$result_query_partial = $this->executarQuery($query, false, [$projeto->id_projeto]);
-				if($result_query_partial){
-					$array_partial = array();
-					foreach($result_query_partial as $key_recursos_projeto => $value_recursos_projeto){
-						$array_partial = array_merge($array_partial, [$key_recursos_projeto => $value_recursos_projeto]);
-					}
-					$result_projeto = array_merge($result_projeto, ["area_atuacao" => $array_partial]);
-				}
-				
-				$query = "SELECT * FROM portal.obter_osc_area_atuacao_outra_projeto(?::INTEGER);";
-				$result_query_partial = $this->executarQuery($query, false, [$projeto->id_projeto]);
-				if($result_query_partial){
-					$array_partial = array();
-					foreach($result_query_partial as $key_recursos_projeto => $value_recursos_projeto){
-						$array_partial = array_merge($array_partial, [$key_recursos_projeto => $value_recursos_projeto]);
-					}
-					$result_projeto = array_merge($result_projeto, ["area_atuacao_outra" => $array_partial]);
-				}
-				
-				$query = "SELECT * FROM portal.obter_osc_localizacao_projeto(?::INTEGER);";
-				$result_query_partial = $this->executarQuery($query, false, [$projeto->id_projeto]);
-				if($result_query_partial){
-					$array_partial = array();
-					foreach($result_query_partial as $key_recursos_projeto => $value_recursos_projeto){
-						$array_partial = array_merge($array_partial, [$key_recursos_projeto => $value_recursos_projeto]);
-					}
-					$result_projeto = array_merge($result_projeto, ["localizacao" => $array_partial]);
-				}
-				
-				$query = "SELECT * FROM portal.obter_osc_parceira_projeto(?::INTEGER);";
-				$result_query_partial = $this->executarQuery($query, false, [$projeto->id_projeto]);
-				if($result_query_partial){
-					$array_partial = array();
-					foreach($result_query_partial as $key_recursos_projeto => $value_recursos_projeto){
-						$array_partial = array_merge($array_partial, [$key_recursos_projeto => $value_recursos_projeto]);
-					}
-					$result_projeto = array_merge($result_projeto, ["osc_parceira" => $array_partial]);
-				}
-				
-				$query = "SELECT * FROM portal.obter_osc_objetivo_projeto(?::INTEGER);";
-				$result_query_partial = $this->executarQuery($query, false, [$projeto->id_projeto]);
-				if($result_query_partial){
-					$array_partial = array();
-					foreach($result_query_partial as $key_recursos_projeto => $value_recursos_projeto){
-						$array_partial = array_merge($array_partial, [$key_recursos_projeto => $value_recursos_projeto]);
-					}
-					$result_projeto = array_merge($result_projeto, ["objetivo_meta" => $array_partial]);
-				}
-				
-				$result_partial = array_merge($result_partial, [$key => $result_projeto]);
-	        }
-			
-        	$result = array_merge($result, ["projeto" => $result_partial]);
-			
-	        if(count($result) == 0){
-	            return null;
-	        }else{
-	            if(count($result) == 0){
-	                return null;
-	            }else{
-		            $query = "SELECT * FROM portal.obter_recursos_projeto(?::TEXT);";
-		        	$result_query = $this->executarQuery($query, true, [$param]);
-
-		        	if($result_query){
-		                $result_partial = array();
-		                foreach($result_query as $key => $value){
-		        			$result_partial = array_merge($result_partial, [$key => $value]);
-		        		}
-		                $result = array_merge($result, ["recursos" => $result_partial]);
-		        	}
-					
-		            if(count($result) == 0){
-		                return null;
-		            }else{
-		                return $result;
-		            }
-	            }
-	        }
-		}
     }
 	
 	private function getProjetoAbreviado($param){
@@ -604,27 +464,32 @@ class OscDao extends DaoPostgres{
     }
     
     private function getRelacoesTrabalhoGovernanca($param){
-    	$result = array();
+		$result = array();
+		
     	$query = "SELECT * FROM portal.obter_osc_relacoes_trabalho(?::TEXT);";
     	$result_query = $this->executarQuery($query, true, [$param]);
     	if($result_query){
     		$result = array_merge($result, ["relacoes_trabalho" => $result_query]);
-    	}
+		}
+		
     	$query = "SELECT * FROM portal.obter_osc_relacoes_trabalho_outra(?::TEXT);";
     	$result_query = $this->executarQuery($query, false, [$param]);
     	if($result_query){
     		$result = array_merge($result, ["relacoes_trabalho_outra" => $result_query]);
-    	}
+		}
+		
     	$query = "SELECT * FROM portal.obter_osc_governanca(?::TEXT);";
     	$result_query = $this->executarQuery($query, false, [$param]);
     	if($result_query){
     		$result = array_merge($result, ["governanca" => $result_query]);
-    	}
+		}
+		
     	$query = "SELECT * FROM portal.obter_osc_conselho_fiscal(?::TEXT);";
     	$result_query = $this->executarQuery($query, false, [$param]);
     	if($result_query){
     		$result = array_merge($result, ["conselho_fiscal" => $result_query]);
-    	}
+		}
+		
         if(count($result) == 0){
             return null;
         }else{
