e40cfc433d2079a2e65062a73ab69df7439a319c
vagnerpraia <vagnerpraia@gmail.com>
2018-04-27 15:04:27 -0300

    Refatoração do obter participação social

--- app/Dao/Osc/ParticipacaoSocialDao.php
+++ app/Dao/Osc/ParticipacaoSocialDao.php
@@ -6,111 +6,113 @@ use App\Dao\DaoPostgres;
 
 class ParticipacaoSocialDao extends DaoPostgres{
     public function obterParticipacaoSocial($modelo){
-		$result = array();
+		$resultado = array();
 		
-		$param = $modelo->id_osc;
-
-    	$query = 'SELECT * FROM portal.obter_osc_participacao_social_conferencia(?::TEXT)';
-    	$result_query = $this->executarQuery($query, false, [$param]);
-    	
-    	$nao_possui_conf = null;
-    	$json_conf = array();
+    	$conferencias = $this->obterParticipacaoSocialConferencia($modelo);
+    	$naoPossuiConferencia = $this->verificarNaoPossuiConferencia($conferencias);
 		
-    	if($result_query){
-    		foreach($result_query as $key => $value){
-    			$cd_conferencia = $value->cd_conferencia;
-    			if($cd_conferencia == 133){
-    				$nao_possui_conf = true;
-    			}else{
-    				array_push($json_conf, $result_query[$key]);
-    			}
-    		}
-			
-    		if(count($json_conf) == 0){
-    			$result = array_merge($result, ["conferencia" => null]);
-    		}else{
-    			$nao_possui_conf = false;
-    			$result = array_merge($result, ["conferencia" => $json_conf]);
-    		}
+    	if($conferencias == null || $naoPossuiConferencia){
+    		$resultado = array_merge($resultado, ["conferencia" => null]);
     	}else{
-    		$result = array_merge($result, ["conferencia" => null]);
+			$resultado = array_merge($resultado, ["conferencia" => $conferencias]);
     	}
 		
-    	$query = 'SELECT * FROM portal.obter_osc_participacao_social_conselho(?::TEXT)';
-    	$result_query_conselho = $this->executarQuery($query, false, [$param]);
-    	
-    	$nao_possui_cons = null;
-    	$json_cons = array();
-    	
-    	if($result_query_conselho){
-    		$result_partial = array();
-    		
-    		foreach($result_query_conselho as $key => $value){
-    			$cd_conselho = $value->cd_conselho;
-    			if($cd_conselho == 105){
-    				$nao_possui_cons = true;
-    			}else{
-    				array_push($json_cons, $result_query_conselho[$key]);
-    			}
-    		}
-    		
-    		if(count($json_cons) == 0){
-    			$result = array_merge($result, ["conselho" => null]);
-    		}else{
-	    		foreach($json_cons as $key => $conselho){
-	    			$result_conselho = array();
-	    			$result_conselho = array_merge($result_conselho, ["conselho" => $conselho]);
-	    			$query = "SELECT * FROM portal.obter_osc_representante_conselho(?::TEXT);";
-	    			$result_query_representante = $this->executarQuery($query, false, [$conselho->id_conselho]);
-	    			if($result_query_representante){
-	    				$result_conselho = array_merge($result_conselho, ["representante" => $result_query_representante]);
-	    			}
-	    			$result_partial = array_merge($result_partial, [$key => $result_conselho]);
-	    		}
-	    		$nao_possui_cons = false;
-	    		$result = array_merge($result, ['conselho' => $result_partial]);
-    		}
+		$conselhos = $this->obterParticipacaoSocialConselho($modelo);
+    	$naoPossuiConselho = $this->verificarNaoPossuiConselho($conselhos);
+		
+    	if($conselhos == null || $naoPossuiConselho){
+    		$resultado = array_merge($resultado, ["conselho" => null]);
     	}else{
-    		$result = array_merge($result, ["conselho" => null]);
+			$resultado = array_merge($resultado, ["conselho" => $conselhos]);
     	}
 		
-    	$query = "SELECT * FROM portal.obter_osc_participacao_social_outra(?::TEXT);";
-	    $result_query = $this->executarQuery($query, false, [$param]);
-	    
-	    $nao_possui_outra = null;
-	    $json_outra = array();
-	    
-	    if($result_query){
-	    	foreach($result_query as $key => $value){
-	    		$nao_possui_outra = $value->bo_nao_possui;
-	    		if(!$nao_possui_outra){
-	    			array_push($json_outra, $result_query[$key]);
-	    		}
-	    	}
-			
-	    	if(count($json_outra) == 0){
-	    		$result = array_merge($result, ["outra" => null]);
-	    	}else{
-	    		$nao_possui_outra = false;
-	    		$result = array_merge($result, ["outra" => $json_outra]);
-	    	}
-	    }else{
-	    	$result = array_merge($result, ["outra" => null]);
-	    }
-    	
-    	if($result){
-    		$result = array_merge($result, ["bo_nao_possui_conferencias" => $nao_possui_conf,
-    										"bo_nao_possui_conselhos" => $nao_possui_cons,
-    										"bo_nao_possui_outros_part" => $nao_possui_outra]);
+		$outras = $this->obterParticipacaoSocialOutra($modelo);
+    	$naoPossuiOutra = $this->verificarNaoPossuiOutra($outras);
+		
+    	if($outras == null || $naoPossuiOutra){
+    		$resultado = array_merge($resultado, ["outra" => null]);
+    	}else{
+			$resultado = array_merge($resultado, ["outra" => $outras]);
     	}
     	
-        if(count($result) == 0){
-            return null;
-        }else{
-            return $result;
-        }
+    	$resultado = array_merge($resultado, ["bo_nao_possui_participacao_social_conferencia" => $naoPossuiConferencia, "bo_nao_possui_participacao_social_conselho" => $naoPossuiConselho, "bo_nao_possui_participacao_social_outra" => $naoPossuiOutra]);
+    	
+        if(count($resultado) == 0){
+            $resultado = null;
+		}
+		
+        return $resultado;
     }
 	
+	public function obterParticipacaoSocialConferencia($modelo){
+		$query = 'SELECT * FROM portal.obter_osc_participacao_social_conferencia(?::TEXT)';
+    	$resultado = $this->executarQuery($query, false, [$modelo->id_osc]);
+    	
+		return $resultado;
+	}
+
+	private function verificarNaoPossuiConferencia($conferencias){
+		$resultado = false;
+		
+		foreach($conferencias as $key => $value){
+			if($value->cd_conferencia == 133){
+				$resultado = true;
+				break;
+			}
+		}
+		
+		return $resultado;
+	}
+	
+	public function obterParticipacaoSocialConselho($modelo){
+		$query = 'SELECT * FROM portal.obter_osc_participacao_social_conselho(?::TEXT)';
+    	$resultadoConselho = $this->executarQuery($query, false, [$modelo->id_osc]);
+    	
+		foreach($resultadoConselho as $key => $conselho){
+			$query = "SELECT * FROM portal.obter_osc_representante_conselho(?::TEXT);";
+			$resultadoRepresentanteConselho = $this->executarQuery($query, false, [$conselho->id_conselho]);
+			
+			if($resultadoRepresentanteConselho){
+				$conselho->representante = $resultadoRepresentanteConselho;
+			}
+		}
+
+		return $resultadoConselho;
+	}
+
+	private function verificarNaoPossuiConselho($conselhos){
+		$resultado = false;
+		
+		foreach($conselhos as $key => $value){
+			if($value->cd_conselho == 105){
+				$resultado = true;
+				break;
+			}
+		}
+		
+		return $resultado;
+	}
+	
+	public function obterParticipacaoSocialOutra($modelo){
+		$query = 'SELECT * FROM portal.obter_osc_participacao_social_outra(?::TEXT)';
+    	$resultado = $this->executarQuery($query, false, [$modelo->id_osc]);
+    	
+		return $resultado;
+	}
+
+	private function verificarNaoPossuiOutra($outras){
+		$resultado = false;
+		
+		foreach($outras as $key => $value){
+			if($value->bo_nao_possui){
+				$resultado = true;
+				break;
+			}
+		}
+		
+		return $resultado;
+	}
+
     public function editarParticipacaoSocial($identificador, $modelo){
     	$fonte = 'Representante de OSC';
 		$tipoIdentificador = 'id_osc';
