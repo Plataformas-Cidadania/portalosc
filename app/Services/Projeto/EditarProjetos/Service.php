7f3bc1b2ae068d5cd1a06521553c271c32a15d4a
vagnerpraia <vagnerpraia@gmail.com>
2018-07-16 17:11:26 -0300

    Acerto no editar projetos

--- app/Services/Projeto/EditarProjetos/Service.php
+++ app/Services/Projeto/EditarProjetos/Service.php
@@ -9,10 +9,26 @@ class Service extends BaseService{
     public function executar(){
         $usuario = $this->requisicao->getUsuario();
         $requisicao = $this->requisicao->getConteudo();
-		$modelo = new Model($requisicao);
-        
+
+        if(isset($requisicao->projeto) || isset($requisicao->projetos)){
+            $modelo = new Model($requisicao);
+        }else{
+            $requisicaoAjustada = new \stdClass();
+            $requisicaoAjustada->id_osc = $requisicao->id_osc;
+            $requisicaoAjustada->bo_nao_possui_projeto = $requisicao->bo_nao_possui_projeto;
+
+            $requisicaoArray = (array) $requisicao;
+            unset($requisicaoArray['id_osc']);
+            unset($requisicaoArray['bo_nao_possui_projeto']);
+
+            $requisicaoAjustada->projetos = [$requisicaoArray];
+            
+            $modelo = new Model($requisicaoAjustada);
+        }
+
         if($modelo->obterCodigoResposta() === 200){
             $projetos = $modelo->obterRequisicao();
+            
             $dao = (new ProjetoDao)->editarProjetos($usuario->id_usuario, $requisicao->id_osc, $projetos);
 		    $this->analisarDao($dao);
         }else{
