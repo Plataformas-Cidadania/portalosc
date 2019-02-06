3723a5c61969e8e793601d0e9480d3322dc6142c
vagnerpraia <vagnerpraia@gmail.com>
2019-02-06 18:50:41 -0200

    Ajuste no SQL do serviço de exportar dados da busca

--- app/Dao/Exportacao/ExportacaoBuscaDao.php
+++ app/Dao/Exportacao/ExportacaoBuscaDao.php
@@ -48,45 +48,216 @@ class ExportacaoBuscaDao extends DaoPostgres{
             $indice = '';
             $apelido = '';
 
-            switch($variavel){
-                case 'idh':
+            switch(strtolower($variavel)){
+                case 'fam_pbf':
                     $indice .= '1';
-                    $apelido = 'idh';
+                    $apelido = 'fam_pbf';
                     break;
 
-                case 'var2':
+                case 'bpc_idos':
                     $indice .= '2';
-                    $apelido = 'param2';
+                    $apelido = 'bpc_idos';
                     break;
 
-                case 'var3':
+                case 'bpc_defi':
                     $indice .= '3';
-                    $apelido = 'param3';
+                    $apelido = 'bpc_defi';
                     break;
 
-                case 'var4':
+                case '13mort1':
+                case 'mort1':
                     $indice .= '4';
-                    $apelido = 'param4';
+                    $apelido = 'mort1';
                     break;
 
-                case 'var5':
+                case '13mort5':
+                case 'mort5':
                     $indice .= '5';
-                    $apelido = 'param5';
+                    $apelido = 'mort5';
                     break;
 
-                case 'var6':
+                case '13pesorur':
+                case 'pesorur':
                     $indice .= '6';
-                    $apelido = 'param6';
+                    $apelido = 'pesorur';
                     break;
 
-                case 'var7':
+                case '13pesourb':
+                case 'pesourb':
                     $indice .= '7';
-                    $apelido = 'param7';
+                    $apelido = 'pesourb';
                     break;
 
-                case 'var8':
+                case '13idhm':
+                case 'idhm':
                     $indice .= '8';
-                    $apelido = 'param8';
+                    $apelido = 'idhm';
+                    break;
+
+                case '13idhm_e':
+                case 'idhm_e':
+                    $indice .= '9';
+                    $apelido = 'idhm_e';
+                    break;
+
+                case '13idhm_l':
+                case 'idhm_l':
+                    $indice .= '10';
+                    $apelido = 'idhm_l';
+                    break;
+
+                case '13idhm_r':
+                case 'idhm_r':
+                    $indice .= '11';
+                    $apelido = '13idhm_r';
+                    break;
+
+                case 'medhab':
+                    $indice .= '12';
+                    $apelido = 'medhab';
+                    break;
+
+                case '13t_analf15m':
+                case 't_analf15m':
+                    $indice .= '13';
+                    $apelido = 't_analf15m';
+                    break;
+                
+                case 'meduca':
+                    $indice .= '14';
+                    $apelido = 'meduca';
+                    break;
+
+                case '13t_fund25m':
+                case 't_fund25m':
+                    $indice .= '15';
+                    $apelido = 't_fund25m';
+                    break;
+
+                case '13t_med25m':
+                case 't_med25m':
+                    $indice .= '16';
+                    $apelido = 't_med25m';
+                    break;
+
+                case '13t_super25m':
+                case 't_super25m':
+                    $indice .= '17';
+                    $apelido = 't_super25m';
+                    break;
+
+                case '13t_fora4a5':
+                case 't_fora4a5':
+                    $indice .= '18';
+                    $apelido = 't_fora4a5';
+                    break;
+
+                case '13t_fora6a14':
+                case 't_fora6a14':
+                    $indice .= '19';
+                    $apelido = 't_fora6a14';
+                    break;
+
+                case '13i_freq_prop':
+                case 'i_freq_prop':
+                    $indice .= '20';
+                    $apelido = 'i_freq_prop';
+                    break;
+
+                case '13t_des18m':
+                case 't_des18m':
+                    $indice .= '21';
+                    $apelido = 't_des18m';
+                    break;
+
+                case '13trabcc':
+                case 'trabcc':
+                    $indice .= '22';
+                    $apelido = 'trabcc';
+                    break;
+
+                case '13trabsc':
+                case 'trabsc':
+                    $indice .= '23';
+                    $apelido = 'trabsc';
+                    break;
+
+                case '13pmpob':
+                case 'pmpob':
+                    $indice .= '24';
+                    $apelido = 'pmpob';
+                    break;
+
+                case '13pind':
+                case 'pind':
+                    $indice .= '25';
+                    $apelido = 'pind';
+                    break;
+
+                case '13ppob':
+                case 'ppob':
+                    $indice .= '26';
+                    $apelido = 'ppob';
+                    break;
+
+                case '13gini':
+                case 'gini':
+                    $indice .= '27';
+                    $apelido = 'gini';
+                    break;
+                
+                case '13rdpc':
+                case 'rdpc':
+                    $indice .= '28';
+                    $apelido = 'rdpc';
+                    break;
+
+                case '13rpob':
+                case 'rpob':
+                    $indice .= '29';
+                    $apelido = 'rpob';
+                    break;
+
+                case '13rmpob':
+                case 'rmpob':
+                    $indice .= '30';
+                    $apelido = 'rmpob';
+                    break;
+
+                case '13rind':
+                case 'rind':
+                    $indice .= '31';
+                    $apelido = 'rind';
+                    break;
+
+                case '13t_mulchefefif014':
+                case 't_mulchefefif014':
+                    $indice .= '32';
+                    $apelido = 't_mulchefefif014';
+                    break;
+
+                case '13t_agua':
+                case 't_agua':
+                    $indice .= '33';
+                    $apelido = 't_agua';
+                    break;
+
+                case '13agua_esgoto':
+                case 'agua_esgoto':
+                    $indice .= '34';
+                    $apelido = 'agua_esgoto';
+                    break;
+
+                case '13t_sluz':
+                case 't_sluz':
+                    $indice .= '35';
+                    $apelido = 't_sluz';
+                    break;
+
+                case '13t_luz':
+                case 't_luz':
+                    $indice .= '36';
+                    $apelido = 't_luz';
                     break;
             }
 
