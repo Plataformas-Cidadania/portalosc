d25aebe0b361041f6e2ffd176f29ded5b5a04f92
vagnerpraia <vagnerpraia@gmail.com>
2019-02-06 18:14:15 -0200

    Ajuste no SQL do serviço de exportar dados da busca

--- app/Dao/Exportacao/ExportacaoBuscaDao.php
+++ app/Dao/Exportacao/ExportacaoBuscaDao.php
@@ -9,54 +9,7 @@ class ExportacaoBuscaDao extends DaoPostgres{
         $listaOsc = '{' . implode(",", $modelo->listaOsc) . '}';
         $variaveisAdicionais = $modelo->variaveisAdicionais;
 
-        $colunasAdicionais = '';
-
-        foreach($variaveisAdicionais as $variavel){
-            $indice = '';
-
-            switch($variavel){
-                case 'idh':
-                    $indice .= '1';
-                    break;
-
-                case 'var2':
-                    $indice .= '2';
-                    break;
-
-                case 'var3':
-                    $indice .= '3';
-                    break;
-
-                case 'var4':
-                    $indice .= '4';
-                    break;
-
-                case 'var5':
-                    $indice .= '5';
-                    break;
-
-                case 'var6':
-                    $indice .= '6';
-                    break;
-
-                case 'var7':
-                    $indice .= '7';
-                    break;
-
-                case 'var8':
-                    $indice .= '8';
-                    break;
-            }
-
-            if($indice){
-                $colunasAdicionais .= ', (
-                    SELECT nr_valor
-                    FROM ipeadata.tb_ipeadata
-                    WHERE cd_indice = ' . $indice . '
-                    AND cd_municipio = d.cd_municipio
-                )';
-            }
-        }
+        $colunasAdicionais = $this->montarSqlColunasAdicionais($variaveisAdicionais);
 
         $query = '
             SELECT
@@ -80,10 +33,73 @@ class ExportacaoBuscaDao extends DaoPostgres{
                 ON b.eduf_cd_uf = c.eduf_cd_uf
             ) AS d
             ON a.id_osc = d.id_osc
-            WHERE a.id_osc = ANY (?::INTEGER[])';
+            WHERE a.id_osc = ANY (?::INTEGER[])
+        ';
 
         $params = [$listaOsc];
 
         return $this->executarQuery($query, false, $params);
+    }
+
+    private function montarSqlColunasAdicionais($variaveisAdicionais){
+        $sql = '';
+
+        foreach($variaveisAdicionais as $variavel){
+            $indice = '';
+            $apelido = '';
+
+            switch($variavel){
+                case 'idh':
+                    $indice .= '1';
+                    $apelido = 'idh';
+                    break;
+
+                case 'var2':
+                    $indice .= '2';
+                    $apelido = 'param2';
+                    break;
+
+                case 'var3':
+                    $indice .= '3';
+                    $apelido = 'param3';
+                    break;
+
+                case 'var4':
+                    $indice .= '4';
+                    $apelido = 'param4';
+                    break;
+
+                case 'var5':
+                    $indice .= '5';
+                    $apelido = 'param5';
+                    break;
+
+                case 'var6':
+                    $indice .= '6';
+                    $apelido = 'param6';
+                    break;
+
+                case 'var7':
+                    $indice .= '7';
+                    $apelido = 'param7';
+                    break;
+
+                case 'var8':
+                    $indice .= '8';
+                    $apelido = 'param8';
+                    break;
+            }
+
+            if($indice){
+                $sql .= ', (
+                    SELECT nr_valor AS ' . $apelido . '
+                    FROM ipeadata.tb_ipeadata
+                    WHERE cd_indice = ' . $indice . '
+                    AND cd_municipio = d.cd_municipio
+                )';
+            }
+        }
+
+        return $sql;
     }
 }
