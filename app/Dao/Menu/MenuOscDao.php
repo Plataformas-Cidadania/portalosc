98799cf2886724cdb57762c46dc3930791d1bc22
vagnerpraia <vagnerpraia@gmail.com>
2019-02-06 20:22:58 -0200

    Criação do serviço de menu do IpeaData

--- app/Dao/Menu/MenuOscDao.php
+++ app/Dao/Menu/MenuOscDao.php
@@ -93,97 +93,105 @@ class MenuOscDao extends DaoPostgres{
         'tipoparceria' => [
             'query' => 'SELECT * FROM syst.dc_tipo_parceria;', 
             'unique' => false
+        ],
+        'ipeadata' => [
+            'query' => 'SELECT * FROM ipeadata.tb_indice ORDER BY cd_indice;',
+            'unique' => false
         ]
     );
     
     private $queriesOscParametro = array(
         'areaatuacao' => [
             'query' => 'SELECT * FROM syst.dc_area_atuacao WHERE cd_area_atuacao = ?::INTEGER;', 
-            'unique' => false
+            'unique' => true
         ],
         'subareaatuacao' => [
             'query' => 'SELECT * FROM syst.dc_subarea_atuacao WHERE cd_area_atuacao = ?::INTEGER;', 
-            'unique' => false
+            'unique' => true
         ],
         'classeatividadeeconomica' => [
             'query' => 'SELECT * FROM syst.dc_classe_atividade_economica WHERE cd_classe_atividade_economica = ?::INTEGER;', 
-            'unique' => false
+            'unique' => true
         ],
         'subclasseatividadeeconomica' => [
             'query' => 'SELECT * FROM syst.dc_subclasse_atividade_economica WHERE cd_classe_atividade_economica = ?::CHARACTER VARYING;', 
-            'unique' => false
+            'unique' => true
         ],
         'certificado' => [
             'query' => 'SELECT * FROM syst.dc_certificado WHERE cd_certificado = ?::INTEGER;', 
-            'unique' => false
+            'unique' => true
         ],
         'conselho' => [
             'query' => 'SELECT * FROM syst.dc_conselho WHERE cd_conselho = ?::INTEGER;', 
-            'unique' => false
+            'unique' => true
         ],
         'conferencia' => [
             'query' => 'SELECT * FROM syst.dc_conferencia WHERE cd_conferencia = ?::INTEGER;', 
-            'unique' => false
+            'unique' => true
         ],
         'naturezajuridica' => [
             'query' => 'SELECT * FROM syst.dc_natureza_juridica WHERE cd_natureza_juridica = ?::INTEGER;', 
-            'unique' => false
+            'unique' => true
         ],
         'situacaoimovel' => [
             'query' => 'SELECT * FROM syst.dc_situacao_imovel WHERE cd_situacao_imovel = ?::INTEGER;', 
-            'unique' => false
+            'unique' => true
         ],
         'tipoparticipacao' => [
             'query' => 'SELECT * FROM syst.dc_tipo_participacao WHERE cd_tipo_participacao = ?::INTEGER;', 
-            'unique' => false
+            'unique' => true
         ],
         'abrangenciaprojeto' => [
             'query' => 'SELECT * FROM syst.dc_abrangencia_projeto WHERE cd_abrangencia_projeto = ?::INTEGER;', 
-            'unique' => false
+            'unique' => true
         ],
         'origemfonterecursososc' => [
             'query' => 'SELECT * FROM syst.dc_origem_fonte_recursos_osc WHERE cd_origem_fonte_recursos_osc = ?::INTEGER;', 
-            'unique' => false
+            'unique' => true
         ],
         'fonterecursososc' => [
             'query' => 'SELECT * FROM syst.dc_fonte_recursos_osc WHERE cd_fonte_recursos_osc = ?::INTEGER;', 
-            'unique' => false
+            'unique' => true
         ],
         'origemfonterecursosprojeto' => [
             'query' => 'SELECT * FROM syst.dc_origem_fonte_recursos_projeto WHERE cd_origem_fonte_recursos_projeto = ?::INTEGER;', 
-            'unique' => false
+            'unique' => true
         ],
         'fonterecursosprojeto' => [
             'query' => 'SELECT * FROM syst.dc_fonte_recursos_projeto WHERE cd_fonte_recursos_projeto = ?::INTEGER;', 
-            'unique' => false
+            'unique' => true
         ],
         'zonaatuacaoprojeto' => [
             'query' => 'SELECT * FROM syst.dc_zona_atuacao_projeto WHERE cd_zona_atuacao_projeto = ?::INTEGER;', 
-            'unique' => false
+            'unique' => true
         ],
         'objetivoprojeto' => [
             'query' => 'SELECT cd_objetivo_projeto, tx_codigo_objetivo_projeto || \'. \' || tx_nome_objetivo_projeto AS tx_nome_objetivo_projeto FROM syst.dc_objetivo_projeto WHERE cd_objetivo_projeto = ?::INTEGER ORDER BY cd_objetivo_projeto;', 
-            'unique' => false
+            'unique' => true
         ],
         'metaprojeto' => [
             'query' => 'SELECT cd_meta_projeto, tx_codigo_meta_projeto || \' \' || tx_nome_meta_projeto AS tx_nome_meta_projeto FROM syst.dc_meta_projeto WHERE cd_meta_projeto = ?::INTEGER ORDER BY cd_meta_projeto;', 
-            'unique' => false
+            'unique' => true
         ],
         'statusprojeto' => [
             'query' => 'SELECT * FROM syst.dc_status_projeto WHERE cd_status_projeto = ?::INTEGER;', 
-            'unique' => false
+            'unique' => true
         ],
         'periodicidadereuniao' => [
             'query' => 'SELECT * FROM syst.dc_periodicidade_reuniao_conselho WHERE cd_periodicidade_reuniao_conselho = ?::INTEGER;', 
-            'unique' => false
+            'unique' => true
         ],
         'formaparticipacaoconferencia' => [
             'query' => 'SELECT * FROM syst.dc_forma_participacao_conferencia WHERE cd_forma_participacao_conferencia = ?::INTEGER;', 
-            'unique' => false
+            'unique' => true
         ],
         'tipoparceria' => [
             'query' => 'SELECT * FROM syst.dc_tipo_parceria WHERE cd_tipo_parceria = ?::INTEGER;', 
-            'unique' => false
+            'unique' => true
+        ],
+        'ipeadata' => [
+            'query' => 'SELECT * FROM ipeadata.tb_indice WHERE cd_indice = ?::INTEGER;',
+            'unique' => true
         ]
     );
     
