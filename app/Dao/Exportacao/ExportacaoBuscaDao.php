bcd46d0ad0d5b3e07468c21451f2c491f2bb4ad2
vagnerpraia <vagnerpraia@gmail.com>
2019-02-09 17:06:20 -0200

    Alteração do serviço de exportação de busca para a utilização da função do banco

--- app/Dao/Exportacao/ExportacaoBuscaDao.php
+++ app/Dao/Exportacao/ExportacaoBuscaDao.php
@@ -7,70 +7,17 @@ use App\Dao\DaoPostgres;
 class ExportacaoBuscaDao extends DaoPostgres{
     public function ExportarBusca($modelo){
         $listaOsc = '{' . implode(",", $modelo->listaOsc) . '}';
-        $variaveisAdicionais = $modelo->variaveisAdicionais;
-
-        $sqlColunasAdicionais = $this->montarSqlColunasAdicionais($variaveisAdicionais);
+        $variaveisAdicionais = '{' . implode(",", $modelo->variaveisAdicionais) . '}';
 
         $query = '
-            SELECT
-                a.id_osc AS id_osc,
-                a.tx_razao_social_osc AS tx_razao_social,
-                b.tx_nome_natureza_juridica AS tx_natureza_juridica,
-                c.tx_nome_classe_atividade_economica AS tx_classe_atividade_economica,
-                d.edmu_nm_municipio AS tx_municipio,
-                d.eduf_nm_uf AS tx_estado
-                ' . $sqlColunasAdicionais . '
-            FROM osc.tb_dados_gerais AS a
-            LEFT JOIN syst.dc_natureza_juridica AS b
-            ON a.cd_natureza_juridica_osc = b.cd_natureza_juridica
-            LEFT JOIN syst.dc_classe_atividade_economica AS c
-            ON a.cd_classe_atividade_economica_osc = c.cd_classe_atividade_economica
-            LEFT JOIN (
-                osc.tb_localizacao AS a
-                INNER JOIN spat.ed_municipio AS b
-                ON a.cd_municipio = b.edmu_cd_municipio
-                INNER JOIN spat.ed_uf AS c
-                ON b.eduf_cd_uf = c.eduf_cd_uf
-            ) AS d
-            ON a.id_osc = d.id_osc
-            WHERE a.id_osc = ANY (?::INTEGER[])
+            SELECT *
+            FROM portal.obter_exportacao_busca(?::INTEGER[], ?::INTEGER[])
         ';
 
-        $params = [$listaOsc];
+        $params = [$listaOsc, $variaveisAdicionais];
         
-        $resultadoQuery = $this->executarQuery($query, false, $params);
+        $resultadoQuery = $this->executarQuery($query, true, $params);
 
         return $resultadoQuery;
-    }
-
-    private function montarSqlColunasAdicionais($variaveisAdicionais){
-        $sql = '';
-
-        foreach($variaveisAdicionais as $idIndice){
-            $apelido = $this->obterApelidoColuna($idIndice);
-
-            $sql .= ', (
-                SELECT nr_valor AS ' . $apelido . '
-                FROM ipeadata.tb_ipeadata
-                WHERE cd_indice = ' . $idIndice . '
-            )';
-        }
-
-        return $sql;
-    }
-
-    private function obterApelidoColuna($idIndice){
-        $query = '
-            SELECT
-                regexp_replace(reverse(split_part(reverse(tx_nome_indice), \' - \', 1)), \'^[0-9]*\', \'\') AS apelido
-            FROM ipeadata.tb_indice
-            WHERE cd_indice = ?::INTEGER
-        ';
-
-        $params = [$idIndice];
-        $resultadoQuery = $this->executarQuery($query, true, $params);
-        $resultado = $resultadoQuery->apelido;
-
-        return $resultado;
     }
 }
--- app/Services/Exportacao/ExportarBusca/Service.php
+++ app/Services/Exportacao/ExportarBusca/Service.php
@@ -13,13 +13,15 @@ class Service extends BaseService{
 		if($modelo->obterCodigoResposta() === 200){
 			$requisicao = $modelo->obterRequisicao();
 	        $resultadoDao = (new ExportacaoBuscaDao())->ExportarBusca($requisicao);
-    	    
-	        if($resultadoDao){
-                $this->resposta->prepararResposta($resultadoDao, 200);
-	        }else{
-	            $mensagem = 'Este menu não existe ou não contém dados cadastrados no banco de dados.';
-	            $this->resposta->prepararResposta(['msg' => $mensagem], 400);
-	        }
+    	    //print_r($resultadoDao);
+			if($resultadoDao->codigo === 200){
+				$resultado = json_decode($resultadoDao->resultado);
+	    	    $this->resposta->prepararResposta($resultado, 200);
+	    	}else{
+				$mensagem = $resultadoDao->mensagem;
+				$codigo = $resultadoDao->codigo;
+	    		$this->resposta->prepararResposta(['msg' => $mensagem], $codigo);
+	    	}
         }else{
             $this->resposta->prepararResposta($modelo->obterMensagemResposta(), $modelo->obterCodigoResposta());
         }
