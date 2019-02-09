8073a3893d8ad02a206fa753fe98acf4186c5a49
vagnerpraia <vagnerpraia@gmail.com>
2019-02-09 17:08:16 -0200

    Remoção de código de teste

--- app/Services/Exportacao/ExportarBusca/Service.php
+++ app/Services/Exportacao/ExportarBusca/Service.php
@@ -13,7 +13,7 @@ class Service extends BaseService{
 		if($modelo->obterCodigoResposta() === 200){
 			$requisicao = $modelo->obterRequisicao();
 	        $resultadoDao = (new ExportacaoBuscaDao())->ExportarBusca($requisicao);
-    	    //print_r($resultadoDao);
+    	    
 			if($resultadoDao->codigo === 200){
 				$resultado = json_decode($resultadoDao->resultado);
 	    	    $this->resposta->prepararResposta($resultado, 200);
