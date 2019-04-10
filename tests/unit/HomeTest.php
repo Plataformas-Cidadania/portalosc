085d4ba0d8354c52dbcca83bf0c1016c53623282
Jonathan Silva <jonathan.costa@coopersystem.com.br>
2019-04-10 11:56:30 -0300

    Teste de Home e AppInfo

---
+++ tests/unit/HomeTest.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Log;
+
+class LoginUserTest extends TestCase
+{
+    /**
+     * GET /
+     *
+     * @return void
+     */
+    public function testPageHome()
+    {
+        try {
+
+            $response = $this->get('/');
+            $response->seeStatusCode(200);
+            echo ("### Home Portal '/' ###.. \n");
+            echo (".### Requisição feita com sucesso OK !!! ### \n \n");
+            echo $response->response->original;
+        } catch (\Exception $e) {
+            Log::warning('Falha ao acessar home "/".' . "\n");
+            echo ("Erro ao acessar /, consulte o log!!!");
+            return die;
+        }
+    }
+
+    /**
+     * GET /api/sobre
+     *
+     * @return void
+     */
+    public function testAppInfo()
+    {
+        try {
+
+            $response = $this->get('/api/sobre');
+            $response->seeStatusCode(200);
+            echo ("### APP Version '/api/sobre' ###.. \n");
+            echo (".### Requisição feita com sucesso OK !!! ### \n \n");
+            echo $response->response->original;
+        } catch (\Exception $e) {
+            Log::warning('Erro a buscar a info do APP "/api/sobre".' . "\n");
+            echo ("Erro ao acessar info do APP /api/sobre, consulte o log!!!");
+            return die;
+        }
+    }
+}
